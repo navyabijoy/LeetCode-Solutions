00088af6b6d9348c8d4ea8f8e74b4ced58b9694a
navya <115248350+navyabijoy@users.noreply.github.com>
2024-03-08 02:10:41 +0530

    Time: 128 ms (54.70%) | Memory: 18.1 MB (46.11%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,12 +4,12 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n=len(nums)
-        slow = 0
-        for fast in range(n):
-            if nums[fast]!=0 and nums[slow]==0:
-                nums[slow],nums[fast]=nums[fast],nums[slow]
+        slow = 0 #two pointer method, slow one traverses after the fast one
+        for fast in range(n): #the fast pointer traverses the array
+            if nums[fast]!=0 and nums[slow]==0: #if the value at slow is 0 and the value at fast is non zero
+                nums[slow],nums[fast]=nums[fast],nums[slow] #swap the values
                
             
-            if nums[slow] != 0:
+            if nums[slow] != 0: #if the value at slow is non zero, increment slow
                 slow +=1
             
