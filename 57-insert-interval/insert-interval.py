c0c70ca62f8cf5155c589629a874e0d6945ecf07
navya <115248350+navyabijoy@users.noreply.github.com>
2024-03-08 21:22:02 +0530

    Time: 80 ms (30.30%) | Memory: 19.9 MB (83.13%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -4,7 +4,6 @@ class Solution:
         newIntervals.sort(key= lambda i: i[0])
         output=[newIntervals[0]]
         for start,end in newIntervals[1:]:
-            
             lastEnd = output[-1][1]
             if lastEnd >= start:
                 output[-1][1] = max(lastEnd,end)
