4aa69f62e803b488f2abfbf799d333cdc849c1d1
navya <115248350+navyabijoy@users.noreply.github.com>
2024-03-10 02:11:06 +0530

    Time: 67 ms (92.59%) | Memory: 19.9 MB (82.34%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,11 +1,11 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        newIntervals = intervals + [newInterval] #add the new interval to the list of the prev ones
-        newIntervals.sort(key= lambda i: i[0]) #sort the intervals based on the numbers at the 0th index
-        output=[newIntervals[0]] #output array will include the first interval beforehand
-        for start,end in newIntervals[1:]: #traverse through the array
-            lastEnd = output[-1][1] #last end refers to the number at the 1st index of the first interval
-            if lastEnd >= start: 
+        newIntervals = intervals + [newInterval]
+        newIntervals.sort(key= lambda i: i[0])
+        output=[newIntervals[0]]
+        for start,end in newIntervals[1:]:
+            lastEnd = output[-1][1]
+            if lastEnd >= start:
                 output[-1][1] = max(lastEnd,end)
             else:
                 output.append([start,end])
