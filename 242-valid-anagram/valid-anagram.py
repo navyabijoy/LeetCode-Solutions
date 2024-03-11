c7342f9bc1d0cc4ea6df30c82032d3635cda3100
navya <115248350+navyabijoy@users.noreply.github.com>
2024-03-11 21:20:22 +0530

    Time: 36 ms (97.43%) | Memory: 16.9 MB (70.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_ascii = [ord(char)for char in s]
-        t_ascii=[ord(char)for char in t]
-        s_ascii.sort()
-        t_ascii.sort()
-        if s_ascii == t_ascii:
-            return True
-        else:
-            return False
+        count=defaultdict(int)
+        for x in s:
+            count[x] += 1
+        for x in t:
+            count[x] -= 1
+        for val in count.values():
+            if val!=0:
+                return False
+        return True
