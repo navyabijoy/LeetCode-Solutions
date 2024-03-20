76a8c847cc5a86f7ccbbbe440cbc3f5c34c99cdf
navya <115248350+navyabijoy@users.noreply.github.com>
2024-03-20 23:33:10 +0530

    Update find-all-anagrams-in-a-string.py

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -4,13 +4,13 @@ class Solution:
         p_counter = Counter(p) #creates a dictionary for the frequency of every alphabet like { "a": 1, "b": 2 , "c": 1}
         left, right =0, 0 #the two sides of the sliding window
         while right < len(s): #while the string goes on
-            p_counter[s[right]] -=1 
-            if right-left+1 == len(p):
-                if all(val == 0 for val in p_counter.values()):
-                    result.append(left)
-                p_counter[s[left]] += 1
+            p_counter[s[right]] -=1 #decrease the value when the char is found in s
+            if right-left+1 == len(p): #window should be equal to size of p
+                if all(val == 0 for val in p_counter.values()): #to make sure all the values in p_counter is 0
+                    result.append(left) #to add element from left
+                p_counter[s[left]] += 1 
                 left +=1
-            right +=1 
+            right +=1 #increase left
         return result
 
 
