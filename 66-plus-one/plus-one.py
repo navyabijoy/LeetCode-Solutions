3629fbd7ec47ca1313b7f17e60e25b9fab794f81
navya <115248350+navyabijoy@users.noreply.github.com>
2024-03-30 00:45:08 +0530

    Time: 36 ms (58.93%) | Memory: 16.5 MB (40.15%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,15 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         n = len(digits)
-        for i in range(n-1,-1,-1): #iterates through the list in reverse order
-            if digits[i] == 9: #if the digit encountered is 9, then replaces 9 with 0
-                digits[i]=0 #9 is replaced with 0
+        for i in range(n-1,-1,-1):
+            if digits[i] == 9:
+                digits[i]=0
             else:
-                digits[i] = digits[i] + 1 #if the digit is a non 9 number then 1 is added
-                return digits #and returns the array as only 1 number is supposed to be incremented
+                digits[i] = digits[i] + 1
+                return digits
      
-        return [1] + digits #this statement happens when the array encountered is an all 9 array, and the resultant array left is [0,0,0] after being replaced by 0, 1 is supposed 
-        #to be added, '+' is used to concatenate the two arrays
+        return [1] + digits
 
 
             
