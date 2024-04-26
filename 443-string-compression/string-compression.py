dbfbcf673f6f4a23e6780c482f2affbeed65a315
navya <115248350+navyabijoy@users.noreply.github.com>
2024-04-27 04:35:21 +0530

    Time: 63 ms (25.59%) | Memory: 16.6 MB (98.38%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,18 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write_index =0
+        read_index=0
+        while read_index < len(chars):
+            curr_char = chars[read_index]
+            count = 0
+            while read_index < len(chars) and chars[read_index] == curr_char:
+                read_index += 1
+                count += 1
+            chars[write_index] = curr_char
+            write_index += 1
+
+            if count >1:
+                for digit in str(count):
+                    chars[write_index] = digit
+                    write_index += 1
+        return write_index
