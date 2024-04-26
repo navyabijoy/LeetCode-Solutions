cac1c2172639a280fd789debf1e3ee34ab41e08d
navya <115248350+navyabijoy@users.noreply.github.com>
2024-04-27 04:36:18 +0530

    Time: 63 ms (25.59%) | Memory: 16.8 MB (30.29%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -11,8 +11,12 @@ class Solution:
             chars[write_index] = curr_char
             write_index += 1
 
-            if count >1:
-                for digit in str(count):
-                    chars[write_index] = digit
+            if count > 1:
+                count_digits = []
+                while count > 0:
+                    count_digits.append(chr(ord('0') + count % 10))
+                    count //= 10
+                while count_digits:
+                    chars[write_index] = count_digits.pop()
                     write_index += 1
         return write_index
