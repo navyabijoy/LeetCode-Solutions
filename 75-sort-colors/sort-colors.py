1ca576ccface68bf4f82597cb3fca4b3046399bd
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-08-10 01:31:49 +0530

    Time: 35 ms (73.75%) | Memory: 16.5 MB (17.05%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -5,20 +5,16 @@ class Solution:
         """
         l=0
         r=len(nums)-1 
-        i=0 #the traversing pointer
+        i=0 
 
-        def swap(i,j): #swap function 
-            tmp = nums[i]
-            nums[i]=nums[j]
-            nums[j] = tmp
-
-        while(i<=r): #the traversing ends if i moves ahead if r
-            if(nums[i]==0): #if the value is 0
-                swap(l,i) #the front pointer and i swap values
-                l+=1 #start pointer is incremented
-            elif(nums[i]==2): #if the value is 2
-                swap(i,r) #the last pointer and i swap value
-                r-=1 #r moves a step back
+        
+        while(i<=r):
+            if(nums[i]==0):
+                nums[l], nums[i] = nums[i], nums[l]  
+                l+=1 
+            elif(nums[i]==2): 
+                nums[i], nums[r] = nums[r] , nums[i]
+                r-=1 
                 i-=1 
-            i+=1 #i is incremented even if the value is 1
+            i+=1 
             
