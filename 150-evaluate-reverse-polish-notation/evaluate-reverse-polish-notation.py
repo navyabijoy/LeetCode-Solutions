1042b5c6403a0ff2b12dcd2c644b1c8e7f512676
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-10-20 13:20:40 +0530

    Time: 3 ms (99.48%) | Memory: 17.9 MB (5.98%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,24 +1,25 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        result = []
-        for i in tokens:
-            if i not in ["/","*","+","-"]:
-                result.append(int(i))
+        stack_pos = 0  # Keeps track of the "stack" position within tokens
+        for i in range(len(tokens)):
+            if tokens[i] not in ["+", "-", "*", "/"]:
+                tokens[stack_pos] = int(tokens[i])
+                stack_pos += 1
             else:
-            
-                num1 = result.pop()
-                num2 = result.pop()
+                # Perform operations using values from tokens (acting as the stack)
+                num1 = tokens[stack_pos - 1]
+                num2 = tokens[stack_pos - 2]
                 
-                if i == '+':
-                    result.append(num2 + num1)
-                elif i == '-':
-                    result.append(num2 - num1)
-
-                elif i == '*':
-                    result.append(num2 * num1)
-
+                if tokens[i] == '+':
+                    tokens[stack_pos - 2] = num2 + num1
+                elif tokens[i] == '-':
+                    tokens[stack_pos - 2] = num2 - num1
+                elif tokens[i] == '*':
+                    tokens[stack_pos - 2] = num2 * num1
                 else:
-                    result.append(int(num2 / num1))
+                    tokens[stack_pos - 2] = int(num2 / num1)
                 
-                
-        return result[0]
+                # Move stack position down by 1 after performing operation
+                stack_pos -= 1
+        
+        return tokens[0]
