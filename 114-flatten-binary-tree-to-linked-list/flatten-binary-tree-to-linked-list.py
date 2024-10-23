c8c982fabe6fc77d183c55b6e033249a28a4f5c6
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-10-24 02:44:13 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (64.82%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -23,3 +23,12 @@ class Solution:
                 temp.left = None
             temp = temp.right
         return temp
+
+    '''
+    Traverse the binary tree, and for each node, check if it has a left child.
+    If the left child exists, find the rightmost node in the left subtree.
+    Point the right pointer of the rightmost node to the right child of the current node.
+    Set the current node’s right pointer to the current node’s left pointer.
+    Set the current node’s left child to NULL.
+    Repeat the steps above until the entire binary tree has been traversed.
+    '''
