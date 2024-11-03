b5ca5777d4bfdfb99b3b31970523c5ff336564b0
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-11-03 23:10:18 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (40.03%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,13 +1,12 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.strip()  # Step 1: Remove leading and trailing whitespaces
+        s = s.strip()  
         if not s:
-            return 0  # Return 0 if the string is empty after stripping
+            return 0  
 
-        sign = 1  # Step 2: Default sign as positive
+        sign = 1  
         index = 0
 
-        # Step 3: Check for sign in the first character
         if s[index] == '-':
             sign = -1
             index += 1
@@ -19,7 +18,6 @@ class Solution:
             res = res * 10 + int(s[index])  # Accumulate digit by digit
             index += 1
 
-        # Step 4: Apply sign and clamp to 32-bit range
         res *= sign
         res = max(-2**31, min(res, 2**31 - 1))
         
