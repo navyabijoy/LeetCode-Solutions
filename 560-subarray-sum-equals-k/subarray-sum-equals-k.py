04dc002ff8a5d7a9c01d3b1507b146ef2af97277
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-11-13 13:50:54 +0530

    Time: 39 ms (38.02%) | Memory: 19.3 MB (98.35%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -3,6 +3,7 @@ class Solution:
         hashmap = {0: 1}
         count = 0
         preSum = 0
+        # O(N x log N) we have taken a simple map so log N, if 
         for i in range(len(nums)):
             preSum += nums[i]
             remove = preSum - k
