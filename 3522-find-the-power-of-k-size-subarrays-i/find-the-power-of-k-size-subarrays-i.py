680a3aa1d3d9d77dcec6c6f14a7cccf340f20cdd
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-11-16 08:28:18 +0530

    Time: 139 ms (6.92%) | Memory: 16.9 MB (5.94%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -1,24 +1,27 @@
 class Solution:
+    
+        
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        '''
-        Time Complexity: O(n * k)
-        Space Complexity: O(n - k + 1)
-        '''
+        
         def ifSorted(nums):
-            '''
-            Helper function to check if the given sub_arr is consecutive and sorted or not
-            '''
             for i in range(len(nums)-1):
-                if nums[i+1] != (nums[i] + 1): 
+                if nums[i+1] != nums[i] + 1:
                     return False
             return True
 
-        result = [-1] * (len(nums) - k + 1)
-        for i in range(len(nums) - k + 1):
-            j = (i + k - 1)
-            sub_arr = nums[i:j+1]
-            # Checking the given constraint for the every possible sub_arr of size (i + k -1)
-            if ifSorted(sub_arr) and len(sub_arr) == len(set(sub_arr)):
-                result[i] = max(sub_arr)
+        n = len(nums)
+        results = []
 
-        return result
+        for i in range(n - k + 1):
+            subarray = nums[i:i + k]
+            unique_sorted = sorted(set(subarray))
+
+            if len(unique_sorted) == k and ifSorted(subarray):
+                results.append(max(subarray))
+            else:
+                results.append(-1)
+
+        return results
+
+    
+            
