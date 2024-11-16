3a5539197d63d7a57d9e066dd31edd358cbf5e36
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-11-16 22:00:00 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (15.63%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,26 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def get_bit(num, bit):
-            temp = (1 << bit)
-            temp = temp & num
-            if temp == 0:
-                return 0
-            return 1
-        subsets = []
-    
-        if not nums:
-            return [[]]
-        else:
-            subsets_count = 2 ** len(nums)
-            for i in range(0, subsets_count):
-                subset = set()
-                for j in range(0, len(nums)):
-                    if get_bit(i, j) == 1 and nums[j] not in subset:
-                        subset.add(nums[j])
-                
-                if i == 0:
-                    subsets.append([])
-                else:
-                    subsets.append(list(subset))
-        return subsets
-        
+        res, sol =[], []
+        n = len(nums)
+
+        def backtrack(i):
+            if i == n: #we have reached the end
+                res.append(sol[:])
+                return
+
+            #dont pick nums[i]
+            backtrack(i+1)
+
+            #pick nums[i]
+            sol.append(nums[i])
+            backtrack(i+1)
+            sol.pop()
+
+
+
+        backtrack(0)
+        return res
