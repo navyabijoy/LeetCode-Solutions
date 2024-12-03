46fb2d01910e7aaaa1183e03a2707764cbfda6bb
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-12-04 02:14:00 +0530

    Time: 112 ms (42.65%) | Memory: 48.9 MB (43.45%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -3,10 +3,15 @@ class Solution:
         final = []
         space_index = 0
         num_spaces = len(spaces)
-        for i, char in enumerate(s):
-            if space_index < num_spaces and i == spaces[space_index]:
-                final.append(' ')
+        # for i, char in enumerate(s):
+        for i in range(len(s)):
+            if space_index < len(spaces) and i == spaces[space_index]:
+                final.append(' ') 
                 space_index += 1
-            final.append(char)
+
+            final.append(s[i])
+            # if space_index < num_spaces and i == spaces[space_index]:
+            #     final.append(' ')
+            # final.append(char)
         return ''.join(final)
 
