ed83bb53404d227aae7ff5d0edbd93b71ddfa462
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-12-12 01:39:52 +0530

    Time: 19 ms (96.15%) | Memory: 31.6 MB (11.84%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -3,15 +3,15 @@ class Solution:
         maxi = float('-inf')
         summ = 0
         start = 0
-        left, right = -1, -1
+        # left, right = -1, -1
         for i in range(len(nums)):
             if summ == 0:
                 start = i
             summ += nums[i]
             if(summ > maxi):
                 maxi = summ
-                left = start
-                right = i
+                # left = start
+                # right = i
             if(summ< 0):
                 summ =0
         return maxi
