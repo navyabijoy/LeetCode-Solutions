dfe90aa8e82495a9402de5dc1b120dbb1b53ab05
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-12-14 02:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (18.34%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -1,16 +1,14 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        store_string = []
+        store = []
         balance = 0
-        for i in range(len(s)):
-            if s[i] == '(':
+        for i in s:
+            if i == "(":
                 if balance > 0:
-                    store_string.append(s[i])
-                balance += 1
+                    store.append(i)
+                balance +=1
             else:
                 if balance > 1:
-                    store_string.append(s[i])
-                balance -= 1
-
-
-        return "".join(store_string)
+                    store.append(i)
+                balance -=1 
+        return "".join(store)
