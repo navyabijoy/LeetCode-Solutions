10bc8d987de9513289110c309c1d334df9ec71d3
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-12-22 23:01:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.45%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}
+
+        def helper(s):
+            if len(s) == 0:
+                return True
+
+            if s in memo:
+                return memo[s]
+
+            for word in wordDict: #traversing through each word in dictionary
+                if s.startswith(word):
+                    if helper(s[len(word):]):
+                        memo[s] = True
+                        return True
+            memo[s] = False
+            return False
+        
+        return helper(s)
