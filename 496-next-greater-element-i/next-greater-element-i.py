bee840117aceb85eb859d0b00388f74d51c9115d
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-12-26 23:30:04 +0530

    Time: 2 ms (49.71%) | Memory: 18.2 MB (6.78%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,37 +1,27 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack = []
+        output = []
         hashmap = {}
-        output= []
-
-        for i in reversed(nums2):
+        stack = []
+        for i in range(len(nums2)-1,-1,-1):
             while stack:
-                if stack[-1] > i:
-                    hashmap[i] = stack[-1]
-                    stack.append(i)
-                    break
-                else:
+                if stack[-1] <= nums2[i]:
                     stack.pop()
+                else:
+                    hashmap[nums2[i]] = stack[-1]
+                    stack.append(nums2[i])
+                    break
             if not stack:
-                hashmap[i] = -1
-                stack.append(i) 
+                hashmap[nums2[i]] = -1
+                stack.append(nums2[i])
         for j in nums1:
-            output.append(hashmap[j])
+            if j in hashmap:
+                output.append(hashmap[j])
         return output
 
-'''
-Initialize an empty stack, hashmap and output array.
 
-Iterate through nums2 in reverse.
 
-For each element in nums2, compare the current element with the last element in the stack:
-If the last element in the stack > current element, the element on top of the stack is the next greater element. Add this to the hashmap. Also push the current element to the stack as it is candidate for future elements.
-If the last element in the stack <= current element, we pop the stack until a greater element is found or the stack is empty.
-If the stack is empty, append -1 to the hashmap as there is no greater element. Push the current element to the stack.
-After processing nums2, the hashmap will contain each element of nums2 as the key, and its next greater element (or -1) as the value.
+            
+            
 
-Iterate through nums1, retrieve the next greater element from the hashmap for each element, and append it to the output list. This is an O(1) operation.
-
-Return the output.
-
-'''
+            
