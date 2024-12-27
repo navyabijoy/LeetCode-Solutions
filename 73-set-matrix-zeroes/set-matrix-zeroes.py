6950d18c25f33d55d1d619420cd9d9fd21f4fdbd
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2024-12-27 06:21:14 +0530

    Time: 7 ms (22.87%) | Memory: 18.5 MB (7.51%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,19 +4,20 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         # better solution
-        rows = [0] * len(matrix)
-        cols = [0] * len(matrix[0])
+        rows = [0] * len(matrix) #O(m)
+        cols = [0] * len(matrix[0]) #O(n)
         for r in range (len(rows)):
-            for c in range(len(cols)):
+            for c in range(len(cols)): #O(m*n)
                 if matrix[r][c] == 0:
                     rows[r] = 1
                     cols[c] = 1
 
         for r in range (len(rows)):
-            for c in range(len(cols)):
+            for c in range(len(cols)): # + O(m*n)
                 if rows[r] == 1 or cols[c]==1:
                     matrix[r][c] = 0
 
-
+        #space: O(m) + O(n)
+        #time: O(2*m*n)
         return matrix
         
