5a2397aaa83d9d67f9d10d63590a86b3b7ca90f2
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-01-02 07:44:05 +0530

    Time: 15 ms (93.12%) | Memory: 49.7 MB (40.21%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -1,40 +1,20 @@
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        left = 0
-        n =  len(words)
-        vowels = {'a','e','i','o','u'}
-        prefix_sum = [0] * (n+1)
+        final = []
+        ans = [0]
+        vowel = ['a','e','i','o','u']
+
+        for word in words:
+            if word[0] in vowel and word[-1] in vowel:
+                ans.append(ans[-1]+1)
+            else:
+                ans.append(ans[-1])
+
+        #now we have the total number of words that start and end with a vowel
         
-        for i in range(n):
-            if words[i][0] in vowels and words[i][-1] in vowels:
-                prefix_sum[i+1] = 1
-
-        for i in range(1,n+1):
-            prefix_sum[i] += prefix_sum[i-1]
-
-        ans = []
-        for l1, r1 in queries:
-            ans.append(prefix_sum[r1+1] - prefix_sum[l1]) 
-
-        
-        return ans
-        # final = []
-        # ans = []
-        # PreCount = 1
-        # i = 0
-
-        # for word in words:
-        #     if word[0] in vowels and word[len(word)-1] in vowels:
-        #         PreCount += 1
-        #     ans.append(PreCount)
-        # #now we have the total number of words that start and end with a vowel
-        
-        # while i < len(queries):
-        #     left = queries[i][0]
-        #     right = queries[i][1]
-        #     final.append(sum(ans[left:right]))
-        #     i += 1
-        # return final
+        for left, right in queries:
+            final.append(ans[right+1] - ans[left])
+        return final
 
 
 
