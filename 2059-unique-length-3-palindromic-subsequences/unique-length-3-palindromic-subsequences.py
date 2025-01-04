737c67f8bf115d431af3ed6f0dd1d7c5da3b446e
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-01-04 06:14:19 +0530

    Time: 122 ms (84.83%) | Memory: 18.2 MB (32.97%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        result = set()
+
+        first = {}
+        last = {}
+        for idx, v in enumerate(s):
+            if v not in first:
+                first[v] = idx
+            last[v] = idx
+        #now we have the first and last occurence of the alphabets
+
+        for char in first:
+            if first[char] < last[char]: # the alphabet has occured more than once
+                middle_char = set(s[first[char]+1:last[char]])
+
+                for m in middle_char:
+                    result.add(char+m+char)
+        return len(result)
