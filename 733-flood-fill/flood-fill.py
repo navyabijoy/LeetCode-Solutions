a877b63d15677ef2aa388b33b791e9b7d1e59e13
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-01-14 11:07:09 +0530

    Time: 3 ms (18.18%) | Memory: 17.8 MB (47.62%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,11 +1,11 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         ROWS, COLS = len(image), len(image[0])
-
-        initial = image[sr][sc] # this is the initial color 
+        ans = image
+        initial = ans[sr][sc] # this is the initial color 
 
         if initial == color :
-            return image
+            return ans
 
         q = deque()
         q.append([sr, sc])
@@ -14,12 +14,12 @@ class Solution:
 
         while q:
             r,c = q.popleft()
-            image[r][c] = color
+            ans[r][c] = color
 
             for dr, dc in directions:
                 row, col = r + dr, c + dc
                 # Check if the neighbor is valid and has the initial color
-                if 0 <= row < ROWS and 0 <= col < COLS and image[row][col] == initial:
+                if 0 <= row < ROWS and 0 <= col < COLS and ans[row][col] == initial:
                     q.append([row, col])
 
-        return image
+        return ans
