96ebb2f97444c9e0cb7d269102a901cfc0a89532
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-01-18 11:19:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (6.21%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,7 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         index = 0
-        for i in range(len(nums)):
+        n = len(nums)
+        for i in range(n):
             if nums[i]!=val:
                 nums[index]=nums[i]
                 index +=1
