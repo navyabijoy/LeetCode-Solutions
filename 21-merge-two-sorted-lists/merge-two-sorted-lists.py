df6011fa7065e3e6621af3acb17da06cebbe0e38
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-01-19 07:09:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (56.60%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,34 +1,29 @@
+# Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
-    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        # Create a dummy node to simplify the merging process
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
-        current = dummy
-        
-        # Pointers for traversing the lists
-        p1 = list1
-        p2 = list2
-        
-        while p1 is not None and p2 is not None:
-            if p1.val < p2.val:
-                current.next = p1
-                p1 = p1.next
+        temp = dummy 
+
+        t1 = list1
+        t2 = list2
+
+        while t1 is not None and t2 is not None:
+            if t1.val < t2.val:
+                temp.next = t1
+                t1 = t1.next
             else:
-                current.next = p2
-                p2 = p2.next
-            current = current.next
+                temp.next = t2
+                t2 = t2.next
+            temp = temp.next
         
-        # If there are remaining nodes in list1
-        if p1 is not None:
-            current.next = p1
-        
-        # If there are remaining nodes in list2
-        if p2 is not None:
-            current.next = p2
-        
-        # The head of the merged list is next to the dummy node
+        if t1 is not None:
+            temp.next = t1
+        if t2 is not None:
+            temp.next = t2
         return dummy.next
+        
+        
