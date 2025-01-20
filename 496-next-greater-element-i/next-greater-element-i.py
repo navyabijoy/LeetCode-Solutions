07b60792280315f73cc0aa23bce772bbca7a9bb3
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-01-21 03:12:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (42.39%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,27 +1,23 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        output = []
-        hashmap = {}
         stack = []
+        res = []
+        track = {}
         for i in range(len(nums2)-1,-1,-1):
             while stack:
                 if stack[-1] <= nums2[i]:
                     stack.pop()
                 else:
-                    hashmap[nums2[i]] = stack[-1]
+                    track[nums2[i]] = stack[-1]
                     stack.append(nums2[i])
                     break
+
             if not stack:
-                hashmap[nums2[i]] = -1
+                track[nums2[i]] = -1
                 stack.append(nums2[i])
-        for j in nums1:
-            if j in hashmap:
-                output.append(hashmap[j])
-        return output
-
-
-
-            
-            
-
-            
+        
+        for j in range(len(nums1)):
+            if nums1[j] in track:
+                res.append(track[nums1[j]])
+        
+        return res
