494c482734c78cd0fea9876cc6e6a9d886538a4e
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-01-22 05:55:51 +0530

    Time: 360 ms (27.50%) | Memory: 18.1 MB (11.75%) - LeetSync

--- 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.py
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.py
@@ -1,21 +1,21 @@
 class Solution:
     def minMovesToMakePalindrome(self, s: str) -> int:
         s = list(s)
-
         moves = 0
-        i,j = 0, len(s)-1
+        i,j = 0, len(s) -1
         while i < j:
             k = j
             while k > i:
                 if s[i] == s[k]:
-                    for m in range(k,j):
+                    for m in range(k, j):
                         s[m], s[m+1] = s[m+1] , s[m]
                         moves += 1
                     j -= 1
                     break
                 k -= 1
-            
-            if  k == i:
-                moves += len(s) // 2 -i
-            i+= 1
+            if i == k:
+                moves += (len(s) // 2) - i
+            i += 1
         return moves
+
+        
