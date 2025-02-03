cb8be4db169ce1d72ac15e3e413bd3c3cecfdd3b
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-04 00:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (67.96%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -4,13 +4,13 @@ class Solution:
             return None
         max_len = inc = dec = 1
         for i in range(len(nums)-1):
-            if nums[i] > nums[i+1]: # curr is greater than next
-                dec += 1
-                inc  = 1
+            if nums[i] > nums[i+1]: # curr is greater than next, so its a decreasing order
+                dec += 1 # we increase the decreasing length
+                inc  = 1 # and reset the increment length
             elif nums[i] < nums[i+1]: # curr is less than next
-                inc += 1
-                dec = 1
+                inc += 1 # we increase the indecrement length
+                dec = 1 # and reset the decrement length
             else: # they are equal
-                inc = dec = 1
-            max_len = max(max_len,inc,dec)
+                inc = dec = 1 # reset both
+            max_len = max(max_len,inc,dec) # choose the max number
         return max_len
