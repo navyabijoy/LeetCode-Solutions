a8a41603757552fec1cb79265886b54955c609ce
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-04 23:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (76.88%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,23 +1,20 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
-        res = []
-        track = {}
-        for i in range(len(nums2)-1,-1,-1):
-            while stack:
-                if stack[-1] <= nums2[i]:
-                    stack.pop()
-                else:
-                    track[nums2[i]] = stack[-1]
-                    stack.append(nums2[i])
-                    break
+        sample = {}
+        for num in reversed(nums2):
+            while stack and stack[-1] <= num:
+                stack.pop() # to get the highest number on the top
+            
+            if stack:
+                sample[num] = stack[-1]
+            else:
+                sample[num] = -1
 
-            if not stack:
-                track[nums2[i]] = -1
-                stack.append(nums2[i])
-        
-        for j in range(len(nums1)):
-            if nums1[j] in track:
-                res.append(track[nums1[j]])
-        
-        return res
+            stack.append(num)
+        final = []
+
+        for num in nums1:
+            if num in sample:
+                final.append(sample[num])
+        return final
