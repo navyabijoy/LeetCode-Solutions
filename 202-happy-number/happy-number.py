774a878cda6690afde28b857baf409b986dffc6b
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-07 02:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.97%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,20 +1,7 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-
-        def SumOfSquare(num):
-            output = 0
-            while num:
-                digit = num % 10  # Access the last digit
-                output += digit ** 2  # Square the digit and add it to the output
-                num = num // 10  # Get the remaining digits
-            return output
-
-        slow = n
-        fast = SumOfSquare(n)
-
-        # We use slow and fast pointers to detect a cycle
-        while fast != 1 and slow != fast:
-            slow = SumOfSquare(slow)  # Move slow one step
-            fast = SumOfSquare(SumOfSquare(fast))  # Move fast two steps
-
-        return fast == 1  # If fast reaches 1, the number is happy
+        seen = set() # to add all the squares
+        while n!= 1 and n not in seen:
+            seen.add(n)
+            n = sum(int(digit)**2 for digit in str(n))
+        return n == 1
