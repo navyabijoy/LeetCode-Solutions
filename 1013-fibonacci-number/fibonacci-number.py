96eb4f893682efc564f3544a6f1fb392dfadd6c0
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-08 08:21:28 +0530

    Time: 43 ms (43.43%) | Memory: 17.6 MB (56.41%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,14 +1,18 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dp = [-1] * (n+1) # create an n+1 size 1D Array
-
         if n <= 1:
             return n
-        # AFTER the base case, check if the value is occupied
-        if dp[n] != -1:
-            return dp[n]
+        dp = [-1] * (n+1) # create an n+1 size 1D Array
+
+        # remove the base case, add the inital values after analyzing the base case
+        dp[0] = 0
+        dp[1] = 1
+        
+        # add a for loop to go through all of n+1
+        for i in range(2,n+1):
+            # remove recursion, store and access it from the database
+            dp[i] =dp[i-1] + dp[i-2]
 
         # agar the value is -1 then store the value of dp[n]
-        dp[n] = self.fib(n-1) + self.fib(n-2)
         return dp[n]
             
