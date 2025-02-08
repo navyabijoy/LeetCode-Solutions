3120dcfdf40c4076d69195da8d00abab3ad1bf07
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-08 10:00:35 +0530

    Time: 4 ms (39.85%) | Memory: 17.8 MB (59.29%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,16 +1,17 @@
 class Solution:
     # bottom up approach -> tabulation
     def helper(self,cost, n):
-        dp = [-1] * (n+1) # step 1: creation
 
-        dp[0] = cost[0] # step 2: analyze base case, add initial values
-        dp[1] = cost[1]
+        prev2 = cost[0] # step 2: analyze base case, add initial values
+        prev1 = cost[1]
         
         # adds the cost of the current step, then chooses the step
         # where the step cost is minimum
         for i in range(2,n):
-            dp[i] = cost[i] + min(dp[i-1],dp[i-2])
-        return min(dp[n-1],dp[n-2])
+            curr = cost[i] + min(prev1,prev2)
+            prev2 = prev1
+            prev1 = curr
+        return min(prev1,prev2)
 
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
