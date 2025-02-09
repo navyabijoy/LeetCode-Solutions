b41b67dde7cca8c254cfe977448c61d35b418de1
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-09 18:01:54 +0530

    Time: 4 ms (8.24%) | Memory: 18 MB (22.74%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,20 +1,17 @@
 class Solution:
-    def helper(self,i,j,dp):
-        if i == 0 and j == 0:
-            return 1 # we have reached the top grid
-        if i < 0 or j < 0:
-            return 0 # out of bounds
-        # step 3: check if the value is already stored
-        if dp[i][j] != -1:
-            return dp[i][j]
-        # we start from the right most corner and then proceed to subtract 
-        up = self.helper(i-1,j,dp)
-        left = self.helper(i,j-1,dp) 
-        dp[i][j] = up + left # step 2: store the value
-        return dp[i][j]
+    def helper(self,r,c):
+        # step 1: define the dp array
+        dp = [[0] *c for _ in range(r)]
 
+        for i in range(r):
+            for j in range(c):
+                if i == 0 and j == 0:
+                    dp[i][j] = 1 #base case
+                else:
+                    up = dp[i-1][j]
+                    left = dp[i][j-1]
+                    dp[i][j] = up + left
+        return dp[r-1][c-1]
 
     def uniquePaths(self, m: int, n: int) -> int:
-        # step 1: define the dp array
-        dp = [[-1]*n for _ in range(m)]
-        return self.helper(m-1,n-1,dp)
+        return self.helper(m,n)
