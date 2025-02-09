d444c3c83fa96b63de6755595fb9dc5d9fe53638
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-09 13:29:36 +0530

    Time: 230 ms (26.05%) | Memory: 27.4 MB (97.24%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,28 +1,24 @@
-from typing import List
-
 class Solution:
-    def helper(self, mat, maxi):
+    def helper(self,mat,maxi):
         rows = len(mat)
         cols = len(mat[0])
-        
-        # Convert the matrix from strings to integers
-        for i in range(rows):
-            for j in range(cols):
-                mat[i][j] = int(mat[i][j])
-                maxi[0] = max(maxi[0],mat[i][j])
+        # convert the string array to int
+        for r in range(rows):
+            for c in range(cols):
+                mat[r][c] = int(mat[r][c])
+                maxi[0] = max(maxi[0],mat[r][c])
 
-        for i in range(1, rows):  # Start from row 1
-            for j in range(1, cols):  # Start from column 1
-                if mat[i][j] == 1:
+        for i in range(1,rows): # start from row 1
+            for j in range(1,cols): # start from column 1
+                if mat[i][j] == 1: 
                     mat[i][j] = 1 + min(mat[i-1][j], mat[i][j-1], mat[i-1][j-1])
-                maxi[0] = max(maxi[0], mat[i][j])  # Update maximum square size
-        
+                maxi[0] = max(maxi[0], mat[i][j])
+        # return maxi[0] 
+
+
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         if not matrix or not matrix[0]:
             return 0
-
         maxi = [0] 
-        
         self.helper(matrix, maxi)
-
-        return maxi[0] ** 2
+        return maxi[0] ** 2 
