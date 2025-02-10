c239d9ef8eff7a5f91a5fc17588ea804e435b7a7
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-10 09:03:27 +0530

    Time: 4 ms (16.41%) | Memory: 17.9 MB (35.10%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,7 +1,10 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        seen = set() # to add all the squares
-        while n!= 1 and n not in seen:
+        # if n <= 9:
+        #     return False
+        seen = set()
+        while n != 1 and n not in seen:
             seen.add(n)
             n = sum(int(digit)**2 for digit in str(n))
         return n == 1
+        
