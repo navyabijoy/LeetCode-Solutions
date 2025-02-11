0650d47e8ad99a1400f4d7bf6b12d2f1725f882b
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-11 12:54:57 +0530

    Time: 13 ms (14.21%) | Memory: 21 MB (60.66%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,20 +1,16 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals:
-            return None
-        intervals.sort()
         result = []
-        
-        result.append(intervals[0])
-        for i in range(1, len(intervals)):
-            last_added_interval = result[-1]
-            cur_start = intervals[i][0]
-            curr_end = intervals[i][1]
-
-            prev_end = last_added_interval[1] # accesses the second value of the last added interval
-
-            if cur_start <= prev_end:
-                result[-1][1] = max(curr_end, prev_end)
+        intervals.sort()
+        result.append(intervals[0]) # append the first interval
+        for i in range(1,len(intervals)):
+            curr_start,curr_end = intervals[i]
+            last_interval = result[-1]
+            prev_end = last_interval[1]
+            
+            if curr_start <= prev_end:
+                result.pop()
+                result.append([last_interval[0],max(curr_end,prev_end)])
             else:
-                result.append([cur_start, curr_end])
+                result.append(intervals[i])
         return result
