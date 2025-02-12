d2620428977261d6c9bb886ca6b5a6ad1321e418
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-13 00:44:50 +0530

    Time: 102 ms (79.24%) | Memory: 34.6 MB (77.58%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,21 +1,17 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        if nums is None:
-            return None
-
-        max_sum = 0
-        current_sum = 0
         left = 0
-        seen = set()
+        max_sum = 0
+        curr_sum = 0
+        seen = set() # to have distinct numbers
         for right in range(len(nums)):
             while nums[right] in seen or len(seen) == k:
                 seen.remove(nums[left])
-                current_sum -= nums[left]
+                curr_sum -= nums[left]
                 left += 1
-            
-            seen.add(nums[right])
-            current_sum += nums[right]
 
-            if len(seen) == k:
-                max_sum = max(current_sum, max_sum)
+            seen.add(nums[right])
+            curr_sum += nums[right]
+            if len(seen)== k:
+                max_sum = max(max_sum, curr_sum)
         return max_sum
