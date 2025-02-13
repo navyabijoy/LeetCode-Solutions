ab8b1eead8a589198d3e9a2ce5c557e218df1e10
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-13 06:32:41 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (53.77%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -6,11 +6,9 @@ class Solution:
         for n in nums:
             new_perms = []
             for p in perms:
-                for i in range(len(p)+1): # to insert in every position
+                for i in range(len(p)+1):
                     p_copy = p.copy()
                     p_copy.insert(i,n)
                     new_perms.append(p_copy)
             perms = new_perms
         return perms
-
-
