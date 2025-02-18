3188a00b176588ea3b03a3cdc05d2bbbc8779036
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-19 00:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.22%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -1,18 +1,20 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-        self.preOrder(root, res)
+
+        def traverse(node):
+            if node is None:
+                return 
+            res.append(node.val)
+            if node.left is not None:
+                traverse(node.left)
+            if node.right is not None:
+                traverse(node.right)
+        traverse(root)
         return res
-        
-    def preOrder(self, root, res):
-        if root is None:
-            return None
-        res.append(root.val)
-        self.preOrder(root.left, res)
-        self.preOrder(root.right, res)
