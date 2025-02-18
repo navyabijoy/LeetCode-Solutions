914eb95a7ed9ca6ecfe5e0921f544239c6cb6010
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-19 00:50:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.94%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -4,9 +4,6 @@ class Solution:
         for i in range(len(s)):
             if stack and s[i] in '0123456789':
                 stack.pop()
-                # stack.pop()
             else:
                 stack.append(s[i])
-
         return "".join(stack)
-            
