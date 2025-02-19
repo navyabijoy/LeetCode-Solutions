5415f934da0f3f114e1e1525dc0a60c9f78ee260
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-20 00:22:17 +0530

    Time: 15 ms (60.01%) | Memory: 20.1 MB (50.90%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,18 +1,25 @@
 class Solution:
-    def helper(self,mat,i,j,dp):
-        if i == 0 and j == 0:
-            return mat[i][j]
-        if i < 0 or j < 0:
-            return float('inf')
-        if dp[i][j] != -1:
-            return dp[i][j]
-        up = mat[i][j] + self.helper(mat,i-1,j,dp)
-        left = mat[i][j] + self.helper(mat,i,j-1,dp)
-        dp[i][j] = min(up,left)
-        return dp[i][j]
+    def helper(self,mat,i,j):
+        dp = [[0] * j for _ in range(i)]
+
+        # if i == 0 and j == 0:
+        #     return mat[i][j]
+        # if i < 0 or j < 0:
+        #     return float('inf')
+        # if dp[i][j] != -1:
+        #     return dp[i;][j]
+        for r in range(i):
+            for c in range(j):
+                if r == 0 and c == 0:
+                    dp[r][c] = mat[0][0]
+                else:
+                    up = mat[r][c] + dp[r-1][c] if r > 0 else float('inf')
+                    left = mat[r][c] + dp[r][c-1] if c > 0 else float('inf')
+                    dp[r][c] = min(up,left)
+
+        return dp[i-1][j-1]
 
     def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        dp = [[-1] * n for _ in range(m)]
-        return self.helper(grid,m-1,n-1,dp)
+        return self.helper(grid,m,n)
