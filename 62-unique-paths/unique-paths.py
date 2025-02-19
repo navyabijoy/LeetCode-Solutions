7a1fcc612afd23e825a6834ddb114d67d0f43e44
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-19 23:18:33 +0530

    Time: 4 ms (8.26%) | Memory: 17.7 MB (88.30%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,17 +1,16 @@
 class Solution:
-    def helper(self,r,c):
-        # step 1: define the dp array
-        dp = [[0] *c for _ in range(r)]
-
-        for i in range(r):
-            for j in range(c):
-                if i == 0 and j == 0:
-                    dp[i][j] = 1 #base case
-                else:
-                    up = dp[i-1][j]
-                    left = dp[i][j-1]
-                    dp[i][j] = up + left
-        return dp[r-1][c-1]
-
+    def helper(self,i,j,dp):
+        if i == 0 and j == 0:
+            return 1
+        if i < 0 or j < 0:
+            return 0
+        if dp[i][j] != -1:
+            return dp[i][j]
+        up = self.helper(i-1,j,dp)
+        left = self.helper(i,j-1,dp)
+        dp[i][j] = up+ left
+        return dp[i][j]
     def uniquePaths(self, m: int, n: int) -> int:
-        return self.helper(m,n)
+        dp =[[-1] * n for _ in range(m)]
+        return self.helper(m-1,n-1,dp)
+        
