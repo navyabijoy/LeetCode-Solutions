bc281501b8323dcc33aea879a05b7eb290644e7c
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-20 00:25:10 +0530

    Time: 15 ms (60.01%) | Memory: 18.7 MB (97.97%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,23 +1,17 @@
 class Solution:
     def helper(self,mat,i,j):
-        dp = [[0] * j for _ in range(i)]
-
-        # if i == 0 and j == 0:
-        #     return mat[i][j]
-        # if i < 0 or j < 0:
-        #     return float('inf')
-        # if dp[i][j] != -1:
-        #     return dp[i;][j]
+        prev = [0] * j
         for r in range(i):
+            curr = [0] * j
             for c in range(j):
                 if r == 0 and c == 0:
-                    dp[r][c] = mat[0][0]
+                    curr[c] = mat[0][0]
                 else:
-                    up = mat[r][c] + dp[r-1][c] if r > 0 else float('inf')
-                    left = mat[r][c] + dp[r][c-1] if c > 0 else float('inf')
-                    dp[r][c] = min(up,left)
-
-        return dp[i-1][j-1]
+                    up = mat[r][c] + prev[c] if r > 0 else float('inf')
+                    left = mat[r][c] + curr[c-1] if c > 0 else float('inf')
+                    curr[c] = min(up,left)
+            prev=  curr
+        return prev[-1]
 
     def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid)
