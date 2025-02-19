cc4dcb6639f77fbdc6aaf63223aa995af2fc07bf
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-20 01:28:46 +0530

    Time: 4 ms (56.45%) | Memory: 19.6 MB (28.28%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,14 +1,18 @@
 class Solution:
-    def helper(self,i,j,n,triangle,dp):
-        if i == n-1:
-            return triangle[i][j]
-        if dp[i][j] != -1:
-            return dp[i][j]
-        down = triangle[i][j] + self.helper(i+1,j,n,triangle,dp)
-        diag = triangle[i][j] + self.helper(i+1,j+1,n,triangle,dp)
-        dp[i][j] = min(down,diag)
-        return dp[i][j]
+    def helper(self,i,j,n,triangle):
+        dp = [[-1] * n for _ in range(len(triangle))]
+    
+        for j in range(n):
+            dp[n-1][j] = triangle[n-1][j]
+            
+        for i in range(n-2,-1,-1):
+            for j in range(i,-1,-1):
+    
+                down = triangle[i][j] + dp[i+1][j]
+                diag = triangle[i][j] + dp[i+1][j+1]
+                dp[i][j] = min(down,diag)
+        return dp[0][0]
+
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
-        dp = [[-1] * n for _ in range(len(triangle))]
-        return self.helper(0,0,n,triangle,dp)
+        return self.helper(0,0,n,triangle)
