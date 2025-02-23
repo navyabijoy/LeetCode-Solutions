452a1fdf30a6d703b0fef04c06ce9908b777192e
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-23 11:47:14 +0530

    Time: 941 ms (5.00%) | Memory: 18.4 MB (35.11%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,20 +1,21 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        minLen = float('inf')
-        minChar = ""
+        if not s or not t:
+            return ""
+
+        min_len = float('inf')
+        min_char = ""
         t_count = Counter(t)
         window_count = Counter()
-
         left = 0
         for right in range(len(s)):
-            window_count[s[right]] +=1 
+            window_count[s[right]] += 1
             while all(window_count[char] >= t_count[char] for char in t_count):
-                if minLen > right - left + 1:
-                    minLen = right - left + 1
-                    minChar = s[left:right+1]
-
+                if min_len > right-left+1:
+                    min_len = right - left + 1
+                    min_char = s[left:right+1]
                 window_count[s[left]] -= 1
                 left += 1
             right += 1
-        return minChar
-
+        return min_char
+            
