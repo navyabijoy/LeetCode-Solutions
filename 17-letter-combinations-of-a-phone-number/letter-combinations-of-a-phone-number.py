4f5b329cf07869aa847dacc0153b414a060ad822
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-02-24 02:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (68.54%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -2,14 +2,30 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        keyboard = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs",'8':"tuv","9":"wxyz"}
-        res = [""] 
-        for digit in digits:
-            comb = keyboard[digit]
-            substr = []
-            for prefix in res:
-                for letter in comb:
-                    substr.append(prefix + letter)
-            res = substr
+        keyboard = {
+                "2": "abc",
+                "3": "def",
+                "4": "ghi",
+                "5": "jkl",
+                "6": "mno",
+                "7": "pqrs",
+                "8": "tuv",
+                "9": "wxyz"
+            }
+        res = []
+
+        def backtrack(index, combination):
+            if index == len(digits):
+                res.append("".join(combination))
+                return
+
+            current_digit = digits[index]
+            letter = keyboard[current_digit]
+            for char in letter:
+                combination.append(char)
+                backtrack(index+1,combination)
+                combination.pop()
+
+        backtrack(0,[])
         return res
-            
+
