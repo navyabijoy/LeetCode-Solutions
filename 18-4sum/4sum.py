1c9219a0aa18c0ea34370f849723b64e80f7c634
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-03-30 00:51:41 +0530

    Time: 2492 ms (5.00%) | Memory: 17.7 MB (84.02%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -4,15 +4,15 @@ class Solution:
         temp = []
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
-                hs = set()
+                # hs = set()
                 for k in range(j+1, len(nums)):
                     total = nums[i] + nums[j] + nums[k]
                     fourth = target - total
-                    if fourth in hs:
+                    if fourth in nums[k+1:]:
                         temp = [nums[i],nums[j],nums[k],fourth]
                         temp.sort()
                         st.add(tuple(temp))
-                    hs.add(nums[k])
+                    # hs.add(nums[k])
         ans = [list(t) for t in st]
         return ans
 
