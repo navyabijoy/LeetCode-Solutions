bb0ab9d8c029e30c78a7456448e9860fee2bc24b
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-04-08 19:34:13 +0530

    Time: 12 ms (45.86%) | Memory: 18.6 MB (99.83%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        if len(arr) == k:
+            return arr
+
+        if x <= arr[0]:
+            return arr[:k]
+
+        if x >= arr[-1]:
+            return arr[-k:]
+
+        find_closest = self.binarySearch(arr,x)
+
+        window_left = find_closest - 1
+        window_right = find_closest
+
+        while (window_right - window_left - 1) < k:
+            if window_left == -1:
+                window_right += 1
+                continue 
+            if window_right == len(arr) or abs(arr[window_left] - x) <= abs(arr[window_right] - x):
+                window_left -= 1
+            else:
+                window_right += 1
+
+        return arr[ window_left + 1 : window_right ]
+    
+    def binarySearch(self,nums, target):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left+right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
