dfedc4d59cf708d31f2a06b264f66259db1df593
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-04-10 02:17:51 +0530

    Time: 17 ms (28.00%) | Memory: 55.4 MB (20.15%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int left = 0, right = people.length - 1;
+        int boat = 0;
+        while(left <= right){
+            if((people[left]+people[right]) <= limit){
+                left++;
+            }
+            right--;
+            boat++;
+        }
+        return boat;
+    }
+}
