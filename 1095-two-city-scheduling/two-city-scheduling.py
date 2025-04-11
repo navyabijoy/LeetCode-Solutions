c638ca245f7cb9c77627805a8495c9890cd02ad2
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-04-12 04:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (10.30%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,8 +1,13 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        total_cost = 0
-        costs.sort(key = lambda x : x[0] - x[1])
-        cost_length = len(costs)
-        for i in range(cost_length//2):
-            total_cost = total_cost + costs[i][0] + costs[cost_length-i-1][1]
-        return total_cost
+        diff = []
+        for c1,c2 in costs:
+            diff.append([c1-c2,c1,c2])
+        diff.sort() # sort the difference between the cost of the city
+        res = 0
+        for i in range(len(diff)):
+            if i < len(diff) // 2:
+                res += diff[i][1] # add the cost of first city
+            else:
+                res += diff[i][2] # add the cost of second city
+        return res
