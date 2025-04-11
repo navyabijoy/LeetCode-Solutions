ec25ec37946c80e79d75e5fd12716a2d854e51ce
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-04-12 05:21:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (82.86%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.py
@@ -1,21 +1,18 @@
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         if startFuel >= target:
-            return 0
-        max_heap = []
-        stops = 0
-        i = 0
-        n = len(stations)
+            return 0 # if the start is already greater than target then no need of visiting refueling stops
+        i,n = 0, len(stations)
         max_distance = startFuel
+        max_heap = []
+        stop = 0
         while max_distance < target:
             if i < n and stations[i][0] <= max_distance:
                 heapq.heappush(max_heap, -stations[i][1])
-                i += 1
-            elif not max_heap: #no stations and we cant reach target
+                i+=1
+            elif not max_heap: # no station is in reachable distance
                 return -1
             else:
                 max_distance += -heapq.heappop(max_heap)
-                stops+=1
-        return stops
-
-        
+                stop += 1
+        return stop
