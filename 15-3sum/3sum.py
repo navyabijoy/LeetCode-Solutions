a15fbe7d6c5ac6d5db14623a4873d0a59e1b8423
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-04-12 01:40:48 +0530

    Time: 575 ms (60.89%) | Memory: 20.6 MB (69.33%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,24 +1,21 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res=[]
         nums.sort()
+        res = []
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            j=i+1
-            k=len(nums)-1
-            while j<k:
+            if i > 0 and nums[i] == nums[i-1]: # skip finding the seqeunce for the same number
+                continue 
+            j = i+1
+            k = len(nums)-1
+            while j < k:
                 total = nums[i] + nums[j] + nums[k]
-                if total > 0:
-                    k-=1
-                elif total <0:
-                    j+=1
+                if total < 0:
+                    j += 1
+                elif total > 0:
+                    k -= 1
                 else:
                     res.append([nums[i],nums[j],nums[k]])
-                    j+=1
-                    while j<k and nums[j] == nums[j-1]:
-                        j+=1
+                    j+=1 # to check the potential numbers for the same number but different sequence, but since we cant allow duplicates
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
         return res
-            
-        
-
