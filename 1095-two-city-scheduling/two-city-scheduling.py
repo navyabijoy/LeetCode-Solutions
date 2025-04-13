a02294f8565d4bdeea0e6c3bbb59c23815b1dfd4
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-04-13 22:33:30 +0530

    Update two-city-scheduling.py

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -3,11 +3,14 @@ class Solution:
         diff = []
         for c1,c2 in costs:
             diff.append([c1-c2,c1,c2])
-        diff.sort() # sort the difference between the cost of the city
+        diff.sort()
+        # sort the difference between the cost of the city
         res = 0
         for i in range(len(diff)):
             if i < len(diff) // 2:
-                res += diff[i][1] # add the cost of first city
+                res += diff[i][1] 
+                # add the cost of first city
             else:
-                res += diff[i][2] # add the cost of second city
+                res += diff[i][2] 
+                # add the cost of second city
         return res
