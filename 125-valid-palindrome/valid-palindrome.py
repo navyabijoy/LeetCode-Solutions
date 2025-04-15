469574b198f55bf51c073fdf78a34fb86b2727ba
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-04-15 23:44:31 +0530

    Time: 7 ms (80.73%) | Memory: 17.9 MB (83.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,8 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newSentence = ""
+        newS = ""
         for c in s:
-            if c.isalnum(): 
-#checks if all the characters in a given string are alphanumeric. Alphanumeric characters include letters (a-z, A-Z) and numbers (0-9).
-                newSentence += c.lower()
-        return newSentence == newSentence[::-1]
+            if c.isalnum():
+                newS += c.lower()
+        return newS == newS[::-1]
