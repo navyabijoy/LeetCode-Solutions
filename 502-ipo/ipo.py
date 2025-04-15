a1cc9c2d7b0521f9596cd23d4affc1457d34731e
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-04-15 20:22:13 +0530

    Time: 471 ms (21.02%) | Memory: 42.4 MB (54.17%) - LeetSync

--- 502-ipo/ipo.py
+++ 502-ipo/ipo.py
@@ -1,23 +1,22 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        capitals_min_heap = []
-        profits_max_heap = []
-        
-        # Insert all capital values with their indices into the min heap
-        for x in range(0, len(capital)):
-            heappush(capitals_min_heap, (capital[x], x))
-        
-        for _ in range(k):
-            # Move all projects that can be started with current capital to the max-heap of profits
-            while capitals_min_heap and capitals_min_heap[0][0] <= w:
-                c, i = heappop(capitals_min_heap)
-                heappush(profits_max_heap, (-profits[i], i))  # Push negative profit to simulate max-heap
+        n = len(profits)
+        minCapital = []
+        availableProjects=[]
+
+        for i in range(len(capital)):
+            heapq.heappush(minCapital, [capital[i],profits[i]]) # creates a min heap
+
+        while k > 0:
+            while minCapital and w >= minCapital[0][0]:
+                cap, prof = heapq.heappop(minCapital)
+                heapq.heappush(availableProjects, -prof)
             
-            if not profits_max_heap:
+            if not availableProjects:
                 break
 
-            # Select the project with the maximum profit
-            profit, _ = heappop(profits_max_heap)
-            w += -profit  # Add the profit to current capital (negating it to positive)
-
+            w += -heapq.heappop(availableProjects)
+            k -= 1
         return w
+            
+
