86d5b024d222c592b57506acca699c5d7a1af460
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-04-19 05:19:18 +0530

    Time: 35 ms (24.13%) | Memory: 18.2 MB (56.51%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,33 +1,50 @@
 class Solution:
+    def solve(self,col,ans,board,n):
+        if col == n: # has traversed all the columns
+            copy = ["".join(row) for row in board]
+            ans.append(copy)
+            return
+
+        for row in range(n):
+            if(self.isSafe(row,col,ans,board,n)): # if the col, row, and diag is safe to place
+                board[row][col] = "Q" # then place the queen
+                self.solve(col+1,ans,board,n) # recursively traverse the next column
+                # then backtrack
+                board[row][col] = "."
+
+
+    def isSafe(self,row,col,ans,board,n):
+        x = row
+        y = col
+
+        # check in the same column
+        while y>=0:
+            if board[x][y] =="Q":
+                return False
+            y-= 1
+
+        x = row
+        y = col
+
+        #check in upper diagonal
+        while x >=  0 and y >= 0:
+            if board[x][y] == "Q":
+                return False
+            x -= 1
+            y -= 1
+
+        x = row
+        y = col
+        while x < n and y >= 0:
+            if board[x][y] == "Q":
+                return False
+            y -=1
+            x +=1
+        return True
+
+
     def solveNQueens(self, n: int) -> List[List[str]]:
-        col = set()
-        posDiag = set()
-        negDiag = set()
-
-        res = []
-        board = [['.'] * n for i in range(n)]
-
-        def backtrack(r):
-            if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
-                return
-            
-            for c in range(n):
-                if c in col or (r+c) in posDiag or (r-c) in negDiag:
-                    continue
-                col.add(c)
-                posDiag.add(r+c)
-                negDiag.add(r-c)
-                board[r][c]="Q"
-
-                backtrack(r+1)
-
-                col.remove(c)
-                posDiag.remove(r+c)
-                negDiag.remove(r-c)
-                board[r][c] = "."
-
-        backtrack(0)
-        return res
-                
+        board = [["."] * n for _ in range(n)]
+        ans= []
+        self.solve(0,ans, board,n)
+        return ans
