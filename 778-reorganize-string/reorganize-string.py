a13838c2a291d94d07cf64007bd089a43013b037
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-05-04 05:08:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.18%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -3,20 +3,16 @@ class Solution:
         hashMap = Counter(s)
         maxHeap = []
         res = []
+        for char,freq in hashMap.items():
+            heapq.heappush(maxHeap, (-freq,char))
 
-        for char, freq in hashMap.items():
-            heapq.heappush(maxHeap, (-freq, char))
-        
-        prev_count, prev_char = 0, ""
-
+        prev_char, prev_count = "",0
         while maxHeap:
             count, char = heapq.heappop(maxHeap)
             res.append(char)
             if prev_count < 0:
-                heapq.heappush(maxHeap, (prev_count, prev_char))
-            
+                heapq.heappush(maxHeap,(prev_count, prev_char))
             prev_count, prev_char = count + 1, char
-
         return "".join(res) if len(res) == len(s) else ""
-        
-        
+            
+                 
