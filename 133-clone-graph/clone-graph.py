a94073b0cf281c5f2d9b71a2f0e7d42544cea896
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-06-06 05:56:38 +0530

    Time: 39 ms (81.47%) | Memory: 18.2 MB (39.51%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def helper(self,root, nodes_completed):
+        if not root:
+            return None
+        
+        copy = Node(root.val) 
+        nodes_completed[root] = copy
+
+        for p in root.neighbors:
+            # if p is already cloned, get extract that 
+            x = nodes_completed.get(p)
+            # if p is not cloned, we recursively clone it
+            if not x:
+                copy.neighbors += [self.helper(p, nodes_completed)]
+            else:
+                copy.neighbors += [x]
+        return copy
+
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        nodes_completed = {}
+        return self.helper(node, nodes_completed)
