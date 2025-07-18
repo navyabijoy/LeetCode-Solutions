8eb36ce0a54368c22a5bcffe199c4d2233d0655f
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-07-18 05:31:04 +0530

    Create 2824-CountPairsWhoseSumisLessthanTarget

---
+++ 2824-CountPairsWhoseSumisLessthanTarget/version/python/2824-CountPairsWhoseSumisLessthanTarget_20250718_053110.py
@@ -0,0 +1,12 @@
+# Last updated: 7/18/2025, 5:31:10 AM
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        count = 0
+        res = []
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if i != j and nums[i] + nums[j] < target:
+                    count += 1
+                    # res.append([nums[i],nums[j]])
+
+        return count
