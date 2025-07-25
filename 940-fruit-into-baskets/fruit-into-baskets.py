567cfd335a1ef902a5e16f6be17ab62853fccbe9
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-07-25 06:33:09 +0530

    Time: 185 ms (53.15%) | Memory: 23.7 MB (16.71%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        baskets = defaultdict(int)
+        collected = 0
+        left = 0
+        for right in range(len(fruits)):
+            baskets[fruits[right]] += 1
+            while len(baskets) > 2:
+                baskets[fruits[left]] -= 1
+                if baskets[fruits[left]] == 0:
+                    del baskets[fruits[left]]
+                left += 1
+            collected = max(collected, right - left + 1)
+        return collected
+
+
+            
