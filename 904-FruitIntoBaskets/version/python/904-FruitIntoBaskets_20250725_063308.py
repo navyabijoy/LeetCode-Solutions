bf17cd8d104551f6cfce07f8771ec0e0171d8ede
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-07-25 06:33:07 +0530

    Create 904-FruitIntoBaskets

---
+++ 904-FruitIntoBaskets/version/python/904-FruitIntoBaskets_20250725_063308.py
@@ -0,0 +1,18 @@
+# Last updated: 7/25/2025, 6:33:08 AM
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        baskets = defaultdict(int)
+        collected = 0
+        left = 0
+        for right in range(len(fruits)):
+            baskets[fruits[right]] += 1
+            while len(baskets) > 2:
+                baskets[fruits[left]] -= 1
+                if baskets[fruits[left]] == 0:
+                    del baskets[fruits[left]]
+                left += 1
+            collected = max(collected, right - left + 1)
+        return collected
+
+
+            
