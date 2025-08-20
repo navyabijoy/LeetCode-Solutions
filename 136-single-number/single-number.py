03565a4c3207cebb7658ceefc5b5da73ce4d6587
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-08-20 06:15:21 +0530

    Time: 15 ms (13.43%) | Memory: 19.7 MB (21.13%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,10 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        result = 0
-        for num in nums:
-            result ^= num
-        return result
-        
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen = {}
+        for i in range(len(nums)):
+            if nums[i] not in seen:
+                seen[nums[i]] = 0
+            seen[nums[i]] += 1
+        for num, freq in seen.items():
+            if freq == 1:
+                return num
