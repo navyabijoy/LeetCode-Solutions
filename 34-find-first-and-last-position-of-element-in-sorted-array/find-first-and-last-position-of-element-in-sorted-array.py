7e17d6646d62b8617ff79724cee6468f8d41c6a9
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-09-22 10:43:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (20.51%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,30 +1,34 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def binary_search(nums, target, is_searching_left):
-            left = 0
-            right = len(nums) - 1
-            idx = -1
-            while left <= right:
-                mid = (left+right) // 2
-                if nums[mid] < target:
-                    left = mid + 1
-                elif nums[mid] > target:
-                    right = mid - 1
-                else:
-                    idx = mid
-                    if is_searching_left:
-                        right = mid-1
-                    else:
-                        left = mid + 1
-            return idx
-        
-        start_index = binary_search(nums, target, True)
-        
-        # If the target is not found, no need to search for the rightmost index
-        if start_index == -1:
-            return [-1, -1]
-        
-        # Find the rightmost index of the target
-        end_index = binary_search(nums, target, False)
-        
-        return [start_index, end_index]
+        l = 0
+        r = len(nums) - 1
+        ans = -1
+        f = self.firstOcc(nums,l,r,ans,target)
+        l = self.lastOcc(nums,l,r,ans,target)
+        return [f, l]
+
+    def firstOcc(self, nums, l, r, ans, target):
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                ans = m
+                r = m - 1
+            
+            elif nums[m] > target:
+                r = m -1
+            else:
+                l = m + 1
+        return ans
+
+    def lastOcc(self, nums, l, r, ans, target):
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                ans = m
+                l = m + 1
+            
+            elif nums[m] > target:
+                r = m -1
+            else:
+                l = m + 1
+        return ans
