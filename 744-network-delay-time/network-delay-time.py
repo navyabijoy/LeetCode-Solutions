bd91275f62c4cf9c9f7f3109a1df41cc96c7e961
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-09-25 02:58:16 +0530

    Time: 364 ms (92.90%) | Memory: 19.6 MB (93.06%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,34 +1,27 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        # step 1. Adjacency list
-        adj = defaultdict(list)
-        
+        adj =  defaultdict(list)
+
         for u,v,w in times:
             adj[u].append((v,w))
-            
 
-        # step 2. create the shortest distance array
-        dist = [float('inf')] * (n+1)
-
-        # set the distance from source node to 0
+        dist = [(float('inf'))] * (n+1)
         dist[k] = 0
 
-        # step 3. create min heap (distance, node)
-        heap = [(0, k)]
+        heap = [(0,k)] # [(distance, node)]
 
         while heap:
-            node_dist, node = heapq.heappop(heap)
-            
-            # skip if we already have a better distance
-            if node_dist > dist[node]:
+            node_distance, node = heapq.heappop(heap)
+
+            if node_distance > dist[node]:
                 continue
 
             for nei, weight in adj[node]:
-                new_dist = node_dist + weight
+                new_dist = node_distance + weight
                 if new_dist < dist[nei]:
                     dist[nei] = new_dist
-                    heapq.heappush(heap, [new_dist, nei])
+                    heapq.heappush(heap, (new_dist, nei))
         
         max_dist = max(dist[1:])
-
         return max_dist if max_dist != float('inf') else -1
+
