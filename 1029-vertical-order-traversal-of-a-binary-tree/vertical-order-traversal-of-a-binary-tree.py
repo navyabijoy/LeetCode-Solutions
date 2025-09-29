1a04a5cfd092a281b995341fe090f3322dd48d38
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-09-30 03:49:45 +0530

    Time: 3 ms (20.25%) | Memory: 18.3 MB (18.20%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -6,34 +6,26 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None:
+        if not root:
             return []
 
-        level_items = defaultdict(list)
-        queue = deque([(root, 0, 0)])
-        min_col = float('inf')
-        max_col = float('-inf')
+        q = deque([(root, 0, 0)])
+        cols = []
 
-        while queue:
-            node, row, col = queue.popleft()
-
-            if col < min_col:
-                min_col = col
-
-            if col > max_col:
-                max_col = col
-
-            level_items[col].append((node.val, row)) 
-            # append the value of the node and row in that column
+        while q:
+            node, col, row = q.popleft()
+            cols.append((col, row, node.val))
 
             if node.left:
-                queue.append((node.left, row + 1, col - 1))
+                q.append((node.left, col - 1, row + 1))
             if node.right:
-                queue.append((node.right, row + 1, col + 1))
-        res = []
-        for level in range(min_col, max_col + 1):
-            items = level_items[level]
-            items.sort(key=lambda x: (x[1], x[0]))
-            items = [val for val,_ in items]
-            res.append(items)
-        return res
+                q.append((node.right, col + 1, row + 1))
+            
+        cols.sort(key=lambda x: (x[0], x[1], x[2]))
+
+        res = defaultdict(list)
+
+        for col, row, val in cols:
+            res[col].append(val)
+
+        return [res[c] for c in sorted(res)]
