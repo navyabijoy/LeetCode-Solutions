f73f45b0f8fd8c3aded66886bc2fdb6e8b1719de
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-04 12:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (45.69%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,23 +1,28 @@
 class Solution:
-    def solve(self,m,n,dp):
-        if m == 0 and n == 0:
-            return 1
-
-        if m < 0 or n < 0:
-            return 0
-
-        if dp[m][n] != -1:
-            return dp[m][n]
+    def solve(self,m,n):
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = 1
         
         direction = [[-1,0],[0,-1]] # the path can only be top, left
-        path = 0
-        for r,c in direction:
-            path += self.solve(m+r,n+c,dp)
-        dp[m][n] = path
-        return dp[m][n] 
+        
+
+        for i in range(m):
+            for j in range(n):
+                
+                if i == 0 and j == 0:
+                    continue # already set the value
+                path = 0
+                for r,c in direction:
+                    new_r = i + r
+                    new_c = j + c
+                    if 0 <= new_r < m and 0 <= new_c < n:
+                        path += dp[new_r][new_c]
+                dp[i][j] = path
+
+        return dp[m-1][n-1] 
 
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[-1 for _ in range(n)] for _ in range(m)]
-        ans = self.solve(m-1,n-1,dp)
+        
+        ans = self.solve(m,n)
         return ans
         
