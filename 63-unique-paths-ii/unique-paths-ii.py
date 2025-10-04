f9ac0c49e599e91d5e6814da2d1a5e56d2d8d475
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-04 18:48:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (70.82%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,18 +1,25 @@
 class Solution:
-    def helper(self, mat, r, c,dp):
-        if r < 0 or c < 0 or mat[r][c] == 1:
+    def solve(self,m,n,matrix,dp):
+        if m < 0 or n < 0:
             return 0
-        if r == 0 and c == 0:
+
+        # when obstacle found, return 0
+        if matrix[m][n] == 1:
+            return 0
+
+        if m == 0 and n == 0:
             return 1
         
-        if dp[r][c] != -1:
-            return dp[r][c]
-        up = self.helper(mat,r-1, c,dp)
-        left = self.helper(mat,r,c-1,dp)
-        dp[r][c] = up + left
-        return dp[r][c]
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
-        dp = [[-1]*n for _ in range(m)]
-        return self.helper(obstacleGrid,m-1,n-1,dp)
+        if dp[m][n] != -1:
+            return dp[m][n]
+
+        up = self.solve(m-1,n,matrix,dp)
+        left = self.solve(m,n-1,matrix,dp)
+        dp[m][n] = up+left
+        return dp[m][n]
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        ans = self.solve(m-1,n-1,grid,dp)
+        return ans
