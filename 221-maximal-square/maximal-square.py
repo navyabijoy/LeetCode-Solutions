666ce97288870f3afec59bbab72201ac6232670f
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-08 01:09:37 +0530

    Time: 457 ms (13.54%) | Memory: 32.9 MB (76.08%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,24 +1,34 @@
 class Solution:
-    def helper(self,mat,maxi):
+    def helper(self,mat,r,c,maxi,dp):
         rows = len(mat)
         cols = len(mat[0])
-        # convert the string array to int
-        for r in range(rows):
-            for c in range(cols):
-                mat[r][c] = int(mat[r][c])
-                maxi[0] = max(maxi[0],mat[r][c])
 
-        for i in range(1,rows): # start from row 1
-            for j in range(1,cols): # start from column 1
-                if mat[i][j] == 1: 
-                    mat[i][j] = 1 + min(mat[i-1][j], mat[i][j-1], mat[i-1][j-1])
-                maxi[0] = max(maxi[0], mat[i][j])
-        # return maxi[0] 
+        # base case, if r and c exceed rows and cols value
+        if r >= rows or c >= cols:
+            return 0
 
+        if dp[r][c] != -1: # step 3: check if its occupied
+            return dp[r][c]
+        
+        right = self.helper(mat,r,c+1,maxi,dp)
+        diag = self.helper(mat,r+1,c+1,maxi,dp)
+        down = self.helper(mat,r+1,c,maxi,dp)
 
-    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if mat[r][c] == '1':
+            dp[r][c] = 1 + min(right,diag,down) # step 2: store the values
+            maxi[0] = max(maxi[0],dp[r][c]) # update the max value
+        else:
+            dp[r][c] = 0
+        return dp[r][c] 
+
+    def maximalSquare(self, matrix) -> int:
         if not matrix or not matrix[0]:
             return 0
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        dp = [[-1] * cols for _ in range(rows)] # step 1:created a 2D DP array
         maxi = [0] 
-        self.helper(matrix, maxi)
+        self.helper(matrix, 0, 0, maxi,dp)
         return maxi[0] ** 2 
