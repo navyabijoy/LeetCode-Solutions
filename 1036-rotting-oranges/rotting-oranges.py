a11115b6fee8051da7ad1a42430397f99077dad4
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-09 01:15:02 +0530

    Time: 3 ms (81.18%) | Memory: 17.8 MB (71.48%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,31 +1,27 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = deque()
-        time, fresh = 0, 0 # to keep track of time and fresh oranges
-        
+        time, fresh = 0, 0
+
         ROWS, COLS = len(grid), len(grid[0])
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 1: # that means the orange is fresh
+                if grid[r][c] == 1: # fresh orange
                     fresh += 1
-                if grid[r][c] == 2: # the rotten orange to the queue
+                if grid[r][c] == 2:
                     q.append([r,c])
         
-        # 4 directions to check
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
-        while q and fresh > 0: # so while q exists and fresh oranges exists
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+        while q and fresh > 0:
             for i in range(len(q)):
                 r,c = q.popleft()
                 for dr, dc in directions:
                     row, col = r + dr, c + dc
-                    if(row < 0 or row == len(grid) or col < 0 or col == COLS or grid[row][col] != 1):
-                        continue 
-                # skip the loop if the row and col is out of bounds and the orange is not fresh to make it rotten
+                    if (row < 0 or row == ROWS or col < 0 or col == COLS or grid[row][col] != 1):
+                        continue
                     grid[row][col] = 2
-                    q.append([row, col])
+                    q.append([row,col])
                     fresh -= 1
             time += 1
         return time if fresh == 0 else -1
 
-        # TC: O(n*m)
-        # SC: O(n*m)
