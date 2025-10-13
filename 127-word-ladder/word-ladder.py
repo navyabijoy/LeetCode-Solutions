ebc70c6a09018b16869aa5fccaf31bfd6f82de93
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-14 00:00:44 +0530

    Time: 197 ms (54.89%) | Memory: 18.9 MB (73.18%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,31 +1,28 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         word_set = set(wordList)
+
         if beginWord in word_set:
             word_set.remove(beginWord)
 
         q = deque()
         q.append((beginWord,1))
-
+        
         while q:
-            word, steps = q.popleft()
-            # found the target
+            word, step = q.popleft()
             if word == endWord:
-                return steps
+                return step
 
-            word_chars = list(word)  
-            # Try changing each character
+            word_char = list(word)
+
             for i in range(len(word)):
-                org_char = word_chars[i] # the word we will be changing to other characters
+                org_char = word_char[i]
                 for ch in "abcdefghijklmnopqrstuvwxyz":
-                    word_chars[i] = ch
-                    next_word=  "".join(word_chars)
-                    if next_word in word_set:
-                        word_set.remove(next_word)
-                        q.append((next_word,steps+1))
-                word_chars[i] = org_char
-
-        # no transformation possible
-        return 0
-
-
+                    word_char[i] = ch
+                    new_word = "".join(word_char)
+                    if new_word in word_set:
+                        word_set.remove(new_word)
+                        
+                        q.append((new_word, step+1))
+                word_char[i] = org_char 
+        return 0   
