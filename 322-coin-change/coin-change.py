8a2c1270688bca7491e6b9afca51bb84d66bde83
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-14 04:18:19 +0530

    Time: 1455 ms (7.43%) | Memory: 21.5 MB (24.68%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,23 +1,23 @@
 class Solution:
-    def solve(self,coins,idx,amt,dp):
-        if amt == 0:
-            return 0
+    def solve(self, coins, amt):
+        n = len(coins)
+        dp = [[float("inf")] * (amt + 1) for _ in range(n + 1)]
 
-        if amt < 0 or idx == len(coins):
-            return float('inf') 
-        
-        if dp[idx][amt] != -1:
-            return dp[idx][amt]
+        for i in range(n + 1):
+            dp[i][0] = 0
+        # for: if amt == 0:
+        #         return 0
 
-        take = 1 + self.solve(coins, idx, amt - coins[idx],dp)
-        not_take = self.solve(coins,idx+1,amt,dp)
+        for i in range(n - 1, -1, -1):
+            for j in range(1, amt + 1):
+                take = float("inf")
+                if j - coins[i] >= 0:
+                    take = 1 + dp[i][j - coins[i]]
+                not_take = dp[i + 1][j]
+                dp[i][j] = min(take, not_take)
 
-        dp[idx][amt] = min(take, not_take)
-        return min(take, not_take)
+        return dp[0][amt]
 
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
-        dp = [[-1] * (amount + 1) for _ in range(n)]
-        ans = self.solve(coins,0,amount,dp)
-        return ans if ans != float('inf') else -1
-    
+        ans = self.solve(coins, amount)
+        return ans if ans != float("inf") else -1
