7a2ca89fe9e01d8e9f517e3e330f33c90fa1ea50
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-13 17:31:32 +0530

    Time: 191 ms (55.29%) | Memory: 19 MB (61.53%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -12,16 +12,18 @@ class Solution:
             # found the target
             if word == endWord:
                 return steps
-            
+
+            word_chars = list(word)  
             # Try changing each character
             for i in range(len(word)):
-                org_char = word[i] # the word we will be changing to other characters
+                org_char = word_chars[i] # the word we will be changing to other characters
                 for ch in "abcdefghijklmnopqrstuvwxyz":
-                    word = word[:i] + ch + word[i+1:]
-                    if word in word_set:
-                        word_set.remove(word)
-                        q.append((word,steps+1))
-                word = word[:i] + org_char + word[i+1:]
+                    word_chars[i] = ch
+                    next_word=  "".join(word_chars)
+                    if next_word in word_set:
+                        word_set.remove(next_word)
+                        q.append((next_word,steps+1))
+                word_chars[i] = org_char
 
         # no transformation possible
         return 0
