1f302a8d8d0dba751ec7a3c627727c605d0791a9
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-17 01:43:37 +0530

    Time: 229 ms (10.57%) | Memory: 18.7 MB (49.10%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,20 +1,21 @@
 class Solution:
-    def solve(self,nums,idx,target,dp):
+    def solve(self,arr,curr_sum,target,i,dp):
+        if i == len(arr) and target == curr_sum:
+            return 1
 
-        if idx == len(nums):
-            return 1 if target == 0 else 0
+        if i == len(arr):
+            return 0
+        
+        if (curr_sum,i) in dp:
+            return dp[(curr_sum,i)]
+        
+        incl = self.solve(arr,curr_sum + arr[i], target,i+1,dp)
+        excle =self.solve(arr,curr_sum - arr[i],target,i+1,dp)
 
-        if (idx, target) in dp:
-            return dp[(idx,target)]
-
-        add = self.solve(nums, idx + 1, target + nums[idx],dp) 
-        sub = self.solve(nums, idx + 1, target - nums[idx],dp) 
-        dp[(idx,target)] = add + sub
-        return dp[(idx,target)]
-
-
+        dp[(curr_sum,i)] = incl + excle
+        return dp[(curr_sum,i)]
 
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = {}
-        ans = self.solve(nums,0,target,dp)
+        dp ={}
+        ans = self.solve(nums,0,target,0,dp)
         return ans
