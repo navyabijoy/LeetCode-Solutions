1eed9c85ab0a0a9aedfbb384073e258ec56baff5
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-17 03:01:59 +0530

    Time: 391 ms (41.53%) | Memory: 29.6 MB (51.88%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,20 +1,23 @@
 class Solution:
     def solve(self, coins, idx, amt):
         dp = [[0] * (amt + 1) for _ in range(len(coins)+1)]
-
-        for i in range(len(coins)+1):
-            dp[i][0] = 1
+        prev = [0] * (amt+1)
+        curr = [0] * (amt+1)
+        
+        prev[0] = 1
 
         for i in range(len(coins)-1,-1,-1):
+            curr[0] = 1
             for j in range(1,amt+1):                    
                 incl = 0
                 if j - coins[i] >= 0:
-                    incl = dp[i][j - coins[i]]
-                excl = dp[i+1][j] 
+                    incl = curr[j - coins[i]]
+                excl = prev[j] 
 
-                dp[i][j] = incl + excl
+                curr[j] = incl + excl
+            prev = curr[:]
 
-        return dp[0][amt]
+        return prev[amt]
 
     def change(self, amount: int, coins: List[int]) -> int:
         
