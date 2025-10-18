86fec1950e6242286defc3c4657becf352be8a8d
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-18 07:44:53 +0530

    Time: 424 ms (82.96%) | Memory: 42.9 MB (58.94%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -10,14 +10,28 @@ class Solution:
             return dp[i][j]
 
         ans = 0
+        res = ""
         if text1[i] == text2[j]:
             ans = 1 + self.solve(text1,text2,i+1,j+1,dp)
+            res = text1[i+1]
         else:
             ans = max(self.solve(text1,text2,i+1,j,dp), self.solve(text1,text2,i,j+1,dp))
         
         dp[i][j] = ans
         return dp[i][j]
 
+    def solveTab(self,text1,text2):
+        dp = [[0] * (len(text2)+1) for _ in range((len(text1)+1))]
+        for i in range(len(text1)-1,-1,-1):
+            for j in range(len(text2)-1,-1,-1):
+                ans = 0
+                if text1[i] == text2[j]:
+                    ans = 1 + dp[i+1][j+1]
+                else:
+                    ans = max(dp[i+1][j], dp[i][j+1])
+                
+                dp[i][j] = ans
+        return dp[0][0]
+
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[-1] * (len(text2)) for _ in range((len(text1)))]
-        return self.solve(text1,text2,0,0,dp)
+        return self.solveTab(text1,text2)
