1e0cea04c53ef6ffce7d4b9e3921fa7dc38b007e
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-20 12:53:30 +0530

    Time: 507 ms (70.97%) | Memory: 17.8 MB (94.37%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,26 @@
+class Solution:
+    def solve(self, text1, text2):
+        n = len(text1)
+        prev = [0] * (n+1)
+        
+        
+        for i in range(len(text1)):
+            curr = [0] * (n+1)
+            for j in range(len(text2)):
+                ans = 0
+                if text1[i] == text2[j]:
+                    curr[j] = 1 + prev[j - 1]
+                else:
+                    curr[j] = max(
+                        prev[j],
+                        curr[j - 1],
+                    )
+
+            prev = curr
+
+        return n - prev[n - 1]
+
+    def minInsertions(self, s: str) -> int:
+        revs = s[::-1]
+
+        return self.solve(s, revs)
