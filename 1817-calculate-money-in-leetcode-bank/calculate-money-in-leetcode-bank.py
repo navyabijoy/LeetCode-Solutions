30aa2a16cee934bee161890c46d3a34ef7ee9866
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-25 23:11:33 +0530

    Time: 3 ms (39.58%) | Memory: 17.8 MB (37.64%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -1,18 +1,17 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        t, ans = n, 0
+        days, ans = n, 0
         i = 0
         while i <= n // 7:
             k = i + 1
-            if t // 7 == 0:
-                for j in range(t):
+            if days // 7 == 0:
+                for _ in range(days):
                     ans += k
                     k += 1
             else:
-                for j in range(7):
+                for _ in range(7):
                     ans += k
                     k += 1
-                t -= 7
+                days -= 7
             i += 1
         return ans
- 
