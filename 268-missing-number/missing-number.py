fe6751e077c973aaab6c06538c5b12b088e11367
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-27 09:30:36 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (34.71%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,21 +1,6 @@
 class Solution:
-    def binary_search(self, arr, target):
-        left= 0
-        right = len(arr) -1
-        while left <= right:
-            mid = (left + right) // 2
-            if arr[mid] == target:
-                return True
-            elif arr[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return False
-
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        nums.sort()
-        for i in range(n+1):
-            if not self.binary_search(nums, i):
-                return i
-        return n
+        expected = n*(n+1)// 2
+        actual = sum(nums)
+        return expected - actual
