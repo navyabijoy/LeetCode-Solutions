b28dbf215bd0b4316ae4e5d891201a48daf472c0
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-10-27 09:13:41 +0530

    Time: 3 ms (65.09%) | Memory: 19.8 MB (14.22%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        seen = {}
-        for i in range(len(nums)):
-            if nums[i] not in seen:
-                seen[nums[i]] = 0
-            seen[nums[i]] += 1
-        for num, freq in seen.items():
+        track = Counter(nums)
+        for num, freq in track.items():
             if freq == 1:
                 return num
