0f755dff4a3d4b13453e8bc10a82966c857c91a3
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-01 02:59:12 +0530

    Time: 52 ms (86.69%) | Memory: 37.6 MB (13.14%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -4,59 +4,20 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    '''
-    1. find the middle
-    2. seperate the middle (left linked list) and middle.next(right LL)
-    3. merge sort both the lists
-    4. combine the two lists
-    '''
-    
-
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-        def findMiddle(head):
-            if head is None or head.next is None:
-                return head
-            slow = head
-            fast = head.next
-            while fast and fast.next:
-                slow = slow.next
-                fast = fast.next.next
-            return slow
-
-        def mergeTwoSortedLinkedLists(head1, head2):
-            dummy = ListNode()
-            curr = dummy
-
-            p1 = head1
-            p2 = head2
-
-            while p1 and p2:
-                if p1.val < p2.val:
-                    curr.next = p1
-                    p1 = p1.next
-                else:
-                    curr.next = p2
-                    p2 = p2.next
-                curr = curr.next
-            if p1 is None:
-                curr.next = p2
-            if p2 is None:
-                curr.next = p1
-            
-            return dummy.next
-
-        if not head or not head.next:
-            return head
-
-        mid = findMiddle(head)
+        if not head:
+            return None
+        arr = []
+        curr = head
+        while curr:
+            arr.append(curr.val)
+            curr = curr.next
         
-        temp2 = mid.next
-        mid.next = None
-        temp1 = head
-
-        temp1 = self.sortList(temp1)
-        temp2 = self.sortList(temp2)
-
-        return mergeTwoSortedLinkedLists(temp1, temp2)
-
+        arr.sort()
+        dummy = ListNode(arr[0])
+        curr = dummy
+        for i in range(1, len(arr)):
+            curr.next = ListNode(arr[i])
+            curr = curr.next
+        return dummy
+        
