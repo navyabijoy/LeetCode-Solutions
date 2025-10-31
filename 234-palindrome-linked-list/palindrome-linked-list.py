ffc9071686cd5adab46df81aca99c6a14a4d3839
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-01 01:18:49 +0530

    Time: 43 ms (28.54%) | Memory: 34.8 MB (83.29%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -4,27 +4,33 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def reverseLL(self, head):
+        prev = None
+        curr = head
+        while curr:
+            after = curr.next
+            curr.next = prev
+            prev = curr
+            curr = after
+        return prev
+
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        def reverse(head):
-            before = None
-            curr = head
-            while curr:
-                after = curr.next
-                curr.next = before
-                before = curr
-                curr = after
-            return before
         slow = head
         fast = head
+        
+        # find middle of LL
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        rev = reverse(slow) #slow was pointing at the middle node of the linked lsit
-        while rev:
-            if head.val != rev.val:
-                return False
-            head = head.next
-            rev  = rev.next
-        return True
-    
         
+        # slow is in the center
+        second_half = self.reverseLL(slow)
+        
+        first_half = head
+        while second_half:
+            if first_half.val != second_half.val:
+                return False
+            first_half = first_half.next
+            second_half = second_half.next
+
+        return True
