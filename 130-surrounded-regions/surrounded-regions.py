f6914caa52629383a8663598c1035d2f45775263
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-06 22:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (22.93%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,45 +1,36 @@
 class Solution:
-    def dfs(self,row,col, vis,mat):
-        vis[row][col] = 1 # mark the cell visited
-        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+    def dfs(self, i, j, vis, mat):
         m = len(mat)
         n = len(mat[0])
-        for dr,dc in directions:
-            nrow = row + dr
-            ncol = col + dc
-            if ( 0 <= nrow < m ) and 0 <= ncol < n and not vis[nrow][ncol] and mat[nrow][ncol] == 'O':
-                self.dfs(nrow,ncol,vis,mat)
+        vis[i][j] = True
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for dr, dc in directions:
+            nrow = i + dr
+            ncol = j + dc
+            if 0 <= nrow < m and 0 <= ncol < n and not vis[nrow][ncol] and mat[nrow][ncol] == "O":
+                self.dfs(nrow, ncol, vis, mat)
 
-    def solve(self, mat: List[List[str]]) -> None:
+    def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        m = len(mat)
-        n = len(mat[0])
-        vis = [[0] * n for _ in range(m)]
+        m = len(board)
+        n = len(board[0])
+        vis = [[False] * n for _ in range(m)]
 
-        # first traverse to find the regions with 0 at the border
-
-        # traverse first and last row
-        for j in range(n):
-            if not vis[0][j] and mat[0][j] == "O":
-                self.dfs(0,j,vis,mat)
-            if not vis[m-1][j] and mat[m-1][j] == "O":
-                self.dfs(m-1,j,vis,mat)
-
-        # traverse first and last col
         for i in range(m):
-            if not vis[i][0] and mat[i][0] == 'O':
-                self.dfs(i,0,vis,mat)
-            if not vis[i][n-1] and mat[i][n-1] =='O':
-                self.dfs(i,n-1,vis,mat)
+            if not vis[i][0] and board[i][0] == "O":
+                self.dfs(i, 0, vis, board)
+            if not vis[i][n - 1] and board[i][n - 1] == "O":
+                self.dfs(i, n - 1, vis, board)
 
-        # convert unvisited 0 -> X
+        for j in range(n):
+            if not vis[0][j] and board[0][j] == "O":
+                self.dfs(0, j, vis, board)
+            if not vis[m - 1][j] and board[m - 1][j] == "O":
+                self.dfs(m - 1, j, vis, board)
+
         for i in range(m):
             for j in range(n):
-                if not vis[i][j] and mat[i][j] == "O":
-                    mat[i][j] = "X"
-        
-            
-        
-        
+                if not vis[i][j] and board[i][j] == "O":
+                    board[i][j] = "X"
