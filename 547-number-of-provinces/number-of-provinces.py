a6a175e1ee24e00885021fb9515d6a6c3a736819
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-06 17:32:08 +0530

    Time: 7 ms (55.77%) | Memory: 19.1 MB (59.14%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,25 +1,22 @@
 class Solution:
-    def dfs(self,i,adj,vis):
-        vis[i] = 1
-        for nei in adj[i]:
-            if vis[nei] == 0:
-                self.dfs(nei,adj,vis)
-
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        m = len(isConnected)
-        n = len(isConnected[0])
+        n = len(isConnected)
         adj = defaultdict(list)
-        for r in range(m):
-            for c in range(n):
-                if isConnected[r][c] == 1 and r != c:
-                    adj[r].append(c)
-                    adj[c].append(r)
-        
-        vis = [0] * m
+        vis = [False] * n
         count = 0
-        for i in range(len(vis)):
-            if vis[i] == 0:
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1 and i != j:
+                    adj[i].append(j)
+                    adj[j].append(i)
+        for i in range(n):
+            if vis[i] != True:
                 count += 1
-                self.dfs(i, adj, vis)
+                self.dfs(i,adj,vis,isConnected, n,count)
         return count
-            
+
+    def dfs(self, node, adj, visited, mat, n, count):
+        visited[node] = True
+        for nei in adj[node]:
+            if not visited[nei]:
+                self.dfs(nei, adj, visited, mat, n, count)
