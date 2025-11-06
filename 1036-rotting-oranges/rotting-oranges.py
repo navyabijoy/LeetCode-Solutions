4fb563e53fcd04991f0693c24bc1cbc596bd5725
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-06 18:21:18 +0530

    Time: 3 ms (81.47%) | Memory: 18 MB (25.65%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,27 +1,32 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
         q = deque()
-        time, fresh = 0, 0
-
-        ROWS, COLS = len(grid), len(grid[0])
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1: # fresh orange
+        fresh = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
                     fresh += 1
-                if grid[r][c] == 2:
-                    q.append([r,c])
-        
-        directions = [[1,0], [-1,0], [0,1], [0,-1]]
-        while q and fresh > 0:
-            for i in range(len(q)):
+                if grid[i][j] == 2:
+                    q.append([i,j]) # pushed all the rotten oranges
+        time = 0
+        vis = [[False] * n for _ in range(m)]
+        directions = [(0,1),(0,-1),(-1,0),(1,0)]
+        while q and fresh != 0:
+            for _ in range(len(q)):
                 r,c = q.popleft()
                 for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    if (row < 0 or row == ROWS or col < 0 or col == COLS or grid[row][col] != 1):
-                        continue
-                    grid[row][col] = 2
-                    q.append([row,col])
-                    fresh -= 1
+                    nrow = r + dr
+                    ncol = c + dc
+                    if 0 <= nrow < m and 0 <= ncol < n and not vis[nrow][ncol] and grid[nrow][ncol] == 1:
+                        vis[nrow][ncol] = True
+                        fresh -= 1
+                        
+                        q.append([nrow,ncol])
             time += 1
+
         return time if fresh == 0 else -1
 
+
+        
