b48cc92b1d3d93f1c1b04c4cbec1bcb98b15a939
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-07 02:07:08 +0530

    Time: 152 ms (39.76%) | Memory: 78.5 MB (15.46%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,35 +1,33 @@
 class Node:
     def __init__(self, key, val):
-        self.val = val
         self.key = key
+        self.val = val
         self.prev = None
         self.next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.capacity = capacity
         self.hashmap = {}
+        self.capacity = capacity
 
         self.head = Node(0,0)
         self.tail = Node(0,0)
         self.head.next = self.tail
         self.tail.prev = self.head
     
+    def _insert_at_head(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next = node
+        node.next.prev = node
+        
     def _remove(self,node):
         node.prev.next = node.next
         node.next.prev = node.prev
-    
-    def _insert_at_head(self, node):
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
 
     def get(self, key: int) -> int:
         if key not in self.hashmap:
             return -1
-        # when we get, remove from the last and bring it forward and return
         node = self.hashmap[key]
         self._remove(node)
         self._insert_at_head(node)
@@ -41,13 +39,11 @@ class LRUCache:
         newNode = Node(key, value)
         self._insert_at_head(newNode)
         self.hashmap[key] = newNode
-
+        
         if self.capacity < len(self.hashmap):
-            # if the capacity is full, we remove the node before tail
             lru = self.tail.prev
             self._remove(lru)
             del self.hashmap[lru.key]
-
 
 
 # Your LRUCache object will be instantiated and called as such:
