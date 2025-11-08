24b59526b8c4dde5fad035c733e81e73a16322a6
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-08 18:17:01 +0530

    Time: 3 ms (22.52%) | Memory: 17.7 MB (88.60%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,16 +1,24 @@
 class Solution:
-    def solve(self, m,n,dp):
-        if m == 0 and n == 0:
-            return 1
-        if m < 0 or n < 0:
-            return 0
-        if dp[m][n] != -1:
-            return dp[m][n]
-        up = self.solve(m-1, n, dp)
-        left = self.solve(m, n-1, dp)
-        dp[m][n] = up + left
-        return dp[m][n]
+    def solve(self, m,n):
+        dp = [[0] * n for _ in range(m)]
+        
+        # dp[0][0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[0][0] = 1
+                else:
+                    up = 0
+                    left = 0
+                    if i > 0:
+                        up = dp[i-1][j] # before dp[0-1][j] -> dp[-1][j] -> wrong
+                    if j > 0:
+                        left = dp[i][j-1] # before dp[i][0-1] -> dp[i][-1] -> wrong
+                    dp[i][j] = up + left
+        
+        return dp[m-1][n-1]
+
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[-1] * n for _ in range(m)]
-        ans = self.solve(m-1,n-1,dp)
+        ans = self.solve(m,n)
         return ans
