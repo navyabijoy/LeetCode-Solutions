36792e4af05bc6f320ed89f00a3df83ca00569ba
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-08 21:57:49 +0530

    Time: 23 ms (30.29%) | Memory: 20.1 MB (53.23%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,20 +1,25 @@
 class Solution:
-    def solve(self, r, c, grid, dp):
-        if r == 0 and c == 0:
-            return grid[0][0]
-        if r < 0 or c < 0:
-            return float('inf')
-        if dp[r][c] != -1:
-            return dp[r][c]
-        up = grid[r][c] + self.solve(r - 1, c, grid, dp)
-        left = grid[r][c] + self.solve(r, c - 1, grid, dp)
-        
-        dp[r][c] = min(up,left)
-        return dp[r][c]
-
-    def minPathSum(self, grid: List[List[int]]) -> int:
+    def solve(self,grid):
         m = len(grid)
         n = len(grid[0])
-        dp = [[-1] * n for _ in range(m)]
-        ans = self.solve(m - 1, n - 1, grid, dp)
+        dp = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[0][0]
+                else:
+                    up = float('inf')
+                    left = float('inf')
+                    if i > 0:
+                        up = grid[i][j] + dp[i - 1][j]
+                    if j > 0:
+                        left = grid[i][j] + dp[i][j - 1]
+                    
+                    dp[i][j] = min(up,left)
+
+        return dp[m-1][n-1]
+
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        ans = self.solve(grid)
         return ans
