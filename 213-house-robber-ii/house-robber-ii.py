7e17c0422ee75a055fc25e209afd13d2d621f3e6
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-08 16:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (59.36%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,33 +1,24 @@
 class Solution:
-    def helper(self,nums):
-        n = len(nums)
-        if n == 1:
+    def solve(self, idx, nums, dp):
+        if idx < 0:
+            return 0
+        if idx == 0:
             return nums[0]
+        if dp[idx] != -1:
+            return dp[idx]
+        pick = self.solve(idx - 2, nums, dp) + nums[idx]
+        not_pick = self.solve(idx - 1, nums, dp)
 
-        prev2 = 0
-        prev1 = nums[0]
-        for i in range(1,n):
-            pick =  nums[i] + prev2
-            not_pick = prev1
-            curr = max(pick, not_pick)
-            prev2 = prev1
-            prev1 = curr
+        dp[idx] = max(pick, not_pick)
+        return dp[idx]
 
-        return prev1
     def rob(self, nums: List[int]) -> int:
-        arr1 = []
-        arr2 = []
         n = len(nums)
         if n == 1:
             return nums[0]
+        dp1 = [-1] * (n-1)
+        ans1 = self.solve(n - 2, nums[:-1], dp1)
 
-        for i in range(n):
-            if i != 0:
-                arr1.append(nums[i])
-            if i != n - 1:
-                arr2.append(nums[i])
-
-        ans1 = self.helper(arr1)
-        ans2 = self.helper(arr2)
-
+        dp2 = [-1] * (n-1)
+        ans2 = self.solve(n - 2, nums[1:], dp2)
         return max(ans1, ans2)
