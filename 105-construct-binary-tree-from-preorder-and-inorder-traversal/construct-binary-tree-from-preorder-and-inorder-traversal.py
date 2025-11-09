51fa0dfaa3310516ea94a17cd406b2b298df5c1a
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-09 18:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (70.69%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,26 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findPosition(self, inorder, ele, n):
-        for i in range(n):
-            if inorder[i] == ele:
-                return i
-        return -1
+    def createMapping(self, inorder, nodeToIndex, n):
+        for idx, num in enumerate(inorder):
+            nodeToIndex[num] = idx
+        return nodeToIndex
 
-    def solve(self, pre, inorder, inorderStart, inorderEnd, n):
+    def solve(self, pre, inorder, inorderStart, inorderEnd, n, nodeToIndex):
         if self.index >= n or inorderStart > inorderEnd:
             return None
         ele = pre[self.index]  # in the initial call, get the root node val
         self.index += 1
         root = TreeNode(ele)  # create the root node
-        pos = self.findPosition(inorder, ele, n)
+        pos = nodeToIndex[ele]
 
-        root.left = self.solve(pre, inorder, inorderStart, pos - 1, n)
-        root.right = self.solve(pre, inorder, pos + 1, inorderEnd, n)
+        root.left = self.solve(pre, inorder, inorderStart, pos - 1, n, nodeToIndex)
+        root.right = self.solve(pre, inorder, pos + 1, inorderEnd, n, nodeToIndex)
         return root
 
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         self.index = 0
         n = len(inorder)
-        ans = self.solve(preorder, inorder, 0, n - 1, n)
+        nodeToIndex = self.createMapping(inorder, {},n)
+        ans = self.solve(preorder, inorder, 0, n - 1, n, nodeToIndex)
         return ans
