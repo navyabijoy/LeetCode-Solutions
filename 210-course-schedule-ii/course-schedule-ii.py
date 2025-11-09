04cf279a53f00966d47b9d1b3c0c31a9cc54a2d3
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-10 03:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (47.73%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,20 +1,16 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
         indegree = [0] * numCourses
+        adj = defaultdict(list)
 
-        for u, v in prerequisites:
-            adj[v].append(u)
-            indegree[u] += 1
-
+        for u,v in prerequisites:
+            adj[u].append(v)
+            indegree[v] += 1
         q = deque()
-
         for i in range(numCourses):
             if indegree[i] == 0:
                 q.append(i)
-
         topo = []
-
         while q:
             node = q.popleft()
             topo.append(node)
@@ -22,7 +18,8 @@ class Solution:
                 indegree[nei] -= 1
                 if indegree[nei] == 0:
                     q.append(nei)
-
-        if len(topo) != numCourses:
-            return []
-        return topo
+        
+        ans = []
+        while topo:
+            ans.append(topo.pop())
+        return ans if len(ans) == numCourses else []
