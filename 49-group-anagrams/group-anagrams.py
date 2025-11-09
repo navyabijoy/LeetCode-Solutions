b8aa161d7703661cc4c5c3dd66ee18f0c8d54226
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-09 23:49:12 +0530

    Time: 19 ms (37.12%) | Memory: 22.9 MB (11.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,7 +3,9 @@ class Solution:
         n = len(strs)
         hashMap = defaultdict(list)
         for word in strs:
-            key = "".join(sorted(word))
-            hashMap[key].append(word)
+            freq = [0] * (26)
+            for c in word:
+                freq[ord(c) - ord('a')] += 1
+            hashMap[tuple(freq)].append(word)
 
         return list(hashMap.values())
