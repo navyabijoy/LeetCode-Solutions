37187c70450932a63853ffecc23c0791aa9cdd03
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-10 03:18:31 +0530

    Time: 5 ms (57.30%) | Memory: 19.1 MB (61.43%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,28 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        n = numCourses
-        in_degree = [0] * n
         adj = defaultdict(list)
+        indegree = [0] * numCourses
+
         for u,v in prerequisites:
             adj[u].append(v)
-            in_degree[v] += 1
-
-        q = deque()
-
-        for i in range(n):
-            if in_degree[i] == 0:
-                q.append(i)
+            indegree[v] += 1
         
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
         topo = []
         while q:
             node = q.popleft()
             topo.append(node)
             for nei in adj[node]:
-                in_degree[nei] -= 1
-                if in_degree[nei] == 0:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
                     q.append(nei)
-        
-        if len(topo) != n:
-            return False
-        else:
-            return True
+        return len(topo) == numCourses
