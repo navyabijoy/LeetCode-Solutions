0dd1110ff6bed045607269b6e128da6465921f2d
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-11 04:07:21 +0530

    Time: 44 ms (75.05%) | Memory: 34.6 MB (45.46%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,23 +1,24 @@
 class Solution:
+    def isPali(self,s,left,right):
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
     def partition(self, s: str) -> List[List[str]]:
-        res, sol =[], []
-        n = len(s)
-        def isPalindrome(s,start,end):
-            while start<=end:
-                if s[start] != s[end]:
-                    return False
-                start += 1
-                end -= 1
-            return True
-        
-        def backtrack(i):
-            if i == n:
-                res.append(sol[:])
+        ans = []
+
+        def backtrack(i, res):
+            if i == len(s):
+                ans.append(res[:])
                 return
-            for c in range(i, n): #to iterate in a specific group of characters
-                if isPalindrome(s, i, c):
-                    sol.append(s[i:c + 1])
-                    backtrack(c+1)
-                    sol.pop()
-        backtrack(0)
-        return res
+            
+            for j in range(i, len(s)):
+                if self.isPali(s,i,j):
+                    res.append(s[i:j+1])
+                    backtrack(j+1,res)
+                    res.pop()
+        backtrack(0,[])
+        return ans
