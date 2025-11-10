0629b7a15671ad15bbb3c3a7b83a64b4f903e76f
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-10 18:51:56 +0530

    Time: 1796 ms (22.18%) | Memory: 33.2 MB (43.68%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,24 +1,28 @@
 class Solution:
-    def solve(self, idx, nums, target, dp):
-        if idx < 0:
-            return False
-        if idx == 0:
-            return nums[idx] == target
-        if target < 0:
-            return False
-        if target == 0:
-            return True
-        if dp[idx][target] != -1:
-            return dp[idx][target]
-        incl = self.solve(idx - 1, nums, target - nums[idx], dp)
-        excl = self.solve(idx - 1, nums, target - 0, dp)
-        dp[idx][target] = incl or excl
-        return dp[idx][target]
+    def solve(self, nums, target):
+        n = len(nums)
+        dp = [[False] * (target + 1) for _ in range(n) ]
+
+        for i in range(n):
+            dp[i][0] = True
+
+        if nums[0] <= target:
+            dp[0][nums[0]] = True
+        
+        for i in range(1,n):
+            for t in range(1, target+1):
+                incl = False
+                if nums[i] < t:
+                    incl = dp[i- 1][t - nums[i]]
+                excl = dp[i - 1][t - 0]
+                dp[i][t] = incl or excl
+
+        return dp[n-1][target]
 
     def canPartition(self, nums: List[int]) -> bool:
         if (sum(nums) % 2 != 0):
             return False
         n = len(nums)
         target = sum(nums) // 2
-        dp = [ [-1] * (target + 1) for _ in range(n) ]
-        return self.solve(n-1, nums, target,dp)
+        
+        return self.solve(nums, target)
