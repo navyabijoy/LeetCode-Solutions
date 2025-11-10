ad755b673a69a69b9d1cdbc38b4a275a0d6d3557
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-10 14:40:54 +0530

    Time: 128 ms (67.58%) | Memory: 78 MB (43.33%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -2,48 +2,57 @@ class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev = None
         self.next = None
+        self.prev = None
 
 class LRUCache:
-    def __init__(self, capacity: int):
-        self.hashmap = {}
-        self.capacity = capacity
 
-        self.head = Node(0,0)
-        self.tail = Node(0,0)
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap = {}  # key: node
+
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
+
         self.head.next = self.tail
         self.tail.prev = self.head
     
-    def _insert_at_head(self, node):
-        node.prev = self.head
+    def add_to_head(self, node): 
         node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
         self.head.next = node
-        node.next.prev = node
         
-    def _remove(self,node):
+    
+    def remove_from_end(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
 
     def get(self, key: int) -> int:
-        if key not in self.hashmap:
+        if not key in self.hashmap:
             return -1
         node = self.hashmap[key]
-        self._remove(node)
-        self._insert_at_head(node)
-        return node.val
+        self.remove_from_end(node)
+        self.add_to_head(node)
+        return node.val   
 
     def put(self, key: int, value: int) -> None:
         if key in self.hashmap:
-            self._remove(self.hashmap[key])
-        newNode = Node(key, value)
-        self._insert_at_head(newNode)
-        self.hashmap[key] = newNode
-        
-        if self.capacity < len(self.hashmap):
+            node = self.hashmap[key]
+            node.val = value
+            self.remove_from_end(node)
+            self.add_to_head(node)
+        else: #if not in the map
+            newNode = Node(key, value)
+            self.hashmap[key] = newNode
+            self.add_to_head(newNode)
+
+        if len(self.hashmap) > self.capacity:
             lru = self.tail.prev
-            self._remove(lru)
+            self.remove_from_end(lru)
             del self.hashmap[lru.key]
+
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
