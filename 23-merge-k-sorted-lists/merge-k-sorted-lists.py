d538a5111051fac632f892281405ce2eb290ec65
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-10 15:50:09 +0530

    Time: 7 ms (89.89%) | Memory: 20.2 MB (71.16%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,29 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import heapq
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if lists is None:
-            return None
-        min_heap = []
-        for lst in lists:
-
-            while lst is not None:
-                min_heap.append(lst.val)
-                lst = lst.next
-
-        heapq.heapify(min_heap)
-        merge = []
-        for i in range(len(min_heap)):
-            merge.append(heapq.heappop(min_heap))
-        # creating a list?
+    def mergeTwo(self, head1, head2):
+        t1 = head1
+        t2 = head2
         dummy = ListNode(-1)
         curr = dummy
-        for i in range(len(merge)):
-            new_node = ListNode(merge[i])
-            temp = new_node
-            curr.next = temp
-            curr = temp
-            temp = temp.next
+        while t1 and t2:
+            if t1.val < t2.val:
+                curr.next = t1
+                t1 = t1.next
+            else:
+                curr.next = t2
+                t2 = t2.next
+            curr = curr.next
+        if t1:
+            curr.next = t1
+        if t2:
+            curr.next = t2
         return dummy.next
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0:
+            return None
+        
+        while len(lists) > 1:
+            merge = []
+            for i in range(0, len(lists), 2): # merging 2 and 2 then combining
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                merge.append(self.mergeTwo(l1,l2))
+            lists = merge
+        return lists[0]
