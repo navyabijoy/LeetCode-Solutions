0e40540a76e1188677540b66803b72b7203133fe
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-11 05:12:14 +0530

    Time: 3 ms (68.72%) | Memory: 18 MB (44.88%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,21 +1,19 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        memo = {}
-
-        def helper(s):
-            if len(s) == 0:
+    def recurse(self, wordSet, idx, s,dp):
+        if idx == len(s):
+            return True
+        if idx in dp:
+            return dp[idx]
+        for j in range(idx, len(s)):
+            if s[idx:j+1] in wordSet and self.recurse(wordSet,j+1,s,dp):
+                dp[idx] = True
                 return True
+        dp[idx] = False
+        return False
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = {}
+        wordSet = set(wordDict)
+        return self.recurse(wordSet, 0, s,dp)
 
-            if s in memo:
-                return memo[s]
 
-            for word in wordDict:
-                #traversing through each word in dictionary
-                if s.startswith(word):
-                    if helper(s[len(word):]):
-                        memo[s] = True
-                        return True
-            memo[s] = False
-            return False
         
-        return helper(s)
