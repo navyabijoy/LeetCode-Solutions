1f8acd26d2e3a97e4d015fe1258704469a913bb0
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-12 05:26:16 +0530

    Time: 241 ms (68.04%) | Memory: 20.1 MB (82.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,20 @@
 class Solution:
-    def dfs(self, row,col,vis,grid):
-        vis[row][col] = 1
-        m = len(grid)
-        n = len(grid[0])
-        directions = [(-1,0),(1,0),(0,1),(0,-1)]
+    def dfs(self, vis, row, col, grid):
+        vis[row][col] = True
+        directions = [ (0,-1), (0,1), (1,0),(-1,0)]
         for dr, dc in directions:
-            nrow = dr + row
-            ncol = dc + col
-            if 0 <= nrow < m and 0 <= ncol < n and not vis[nrow][ncol] and grid[nrow][ncol] == "1":
-                self.dfs(nrow,ncol,vis,grid)
-
+            nrow = row + dr
+            ncol = col + dc
+            if 0 <= nrow < len(grid) and 0 <= ncol < len(grid[0]) and not vis[nrow][ncol] and grid[nrow][ncol] == "1":
+                self.dfs(vis, nrow, ncol, grid)
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        vis = [[0] * n for _ in range(m)]
         count = 0
-
+        vis = [[False] * n for _ in range(m)]
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1" and not vis[i][j]:
                     count += 1
-                    self.dfs(i,j,vis,grid)
-        
+                    self.dfs(vis, i, j, grid)
         return count
-        
