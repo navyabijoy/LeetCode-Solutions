a5d53a400d8162a1087bd81ab3bbb8cb87087ac3
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-11 14:52:54 +0530

    Time: 3 ms (74.37%) | Memory: 18.4 MB (63.01%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -2,20 +2,25 @@ class Solution:
     def calculate_profit(self, prices):
         n = len(prices)
         dp = [[0] * 2 for _ in range(n+2)]
-
+        
+        next1_dp = [0,0]
+        next2_dp = [0,0]
+        curr_dp = [0, 0]
         for i in range(n-1,-1,-1):
             for j in range(2):
                 profit = 0
                 if j == 1:
-                    buy_share = -prices[i] + dp[i+ 1][0]
-                    skip_share = 0 + dp[i+ 1][1]
+                    buy_share = -prices[i] + next1_dp[0]
+                    skip_share = 0 + next1_dp[1]
                     profit = max(buy_share, skip_share)
                 else:
-                    sell_share = +prices[i] + dp[i + 2][1]
-                    skip_share = 0 + dp[i+1][0]
+                    sell_share = +prices[i] + next2_dp[1]
+                    skip_share = 0 + next1_dp[0]
                     profit = max(sell_share, skip_share)
-                dp[i][j] = profit
-        return dp[0][1]
+                curr_dp[j] = profit
+            next2_dp = next1_dp[:]
+            next1_dp = curr_dp[:]
+        return next1_dp[1]
 
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
