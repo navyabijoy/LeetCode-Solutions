f9ff27084a467c51b95c09075c70cd75368d1c28
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-11 13:55:39 +0530

    Time: 19 ms (5.87%) | Memory: 21.1 MB (8.32%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,26 +1,27 @@
 class Solution:
-    def calculate_profit(self, prices, index, buy, dp):
-        if index == len(prices):
-            return 0
-        # if index == 0:
-        #     return profit
-        if dp[index][buy] != -1:
-            return dp[index][buy]
-        profit = 0
-        if buy:
-            buy_share = -prices[index] + self.calculate_profit(prices, index + 1, False, dp)
-            not_buy = 0 + self.calculate_profit(prices, index + 1, True, dp)
-            profit = max(buy_share, not_buy)
+    def calculate_profit(self, prices):
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n+1)]
 
-        else:  # we can not buy , as we are supposed to sell now
-            sell_share = +prices[index] + self.calculate_profit(prices, index + 1, True, dp)
-            not_sell = 0 + self.calculate_profit(prices, index + 1, False, dp)
-            profit = max(sell_share, not_sell)
-        dp[index][buy] = profit
-        return dp[index][buy]
+        dp[n][0] = 0
+        dp[n][1] = 0
+        for i in range(n-1,-1,-1):
+            for j in range(2):
+                profit = 0
+                if j == 1:
+                    buy_share = -prices[i] + dp[i + 1][0]
+                    not_buy = 0 + dp[i+1][1]
+                    profit = max(buy_share, not_buy)
+
+                else:  # we can not buy , as we are supposed to sell now
+                    sell_share = prices[i] + dp[i+1][1]
+                    not_sell = 0 + dp[i+1][0]
+                    profit = max(sell_share, not_sell)
+                dp[i][j] = profit
+        return dp[0][1]
 
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[-1] * 2 for _ in range(n)]
-        ans = self.calculate_profit(prices, 0, True, dp)
+        
+        ans = self.calculate_profit(prices)
         return ans
