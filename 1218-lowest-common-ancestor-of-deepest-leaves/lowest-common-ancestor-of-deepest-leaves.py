ce68937fd55df93bcb75a378ab84dfcc3052c242
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2025-11-11 18:58:19 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (92.84%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
@@ -11,13 +11,17 @@ class Solution:
 
         left_node, left_depth = self.dfs(node.left)
         right_node, right_depth = self.dfs(node.right)
-
+        
+         # if left subtree is deeper → answer comes from left
         if left_depth > right_depth:
             return left_node, left_depth + 1
-        elif left_depth < right_depth:
+        
+        # if right subtree is deeper → answer comes from right
+        if left_depth < right_depth:
             return right_node, right_depth + 1
-        else:
-            return node, left_depth + 1  # both equal, this node is LCA
+            
+        # if both sides have same max depth → this node is their LCA   
+        return node, left_depth + 1  # both equal, this node is LCA
 
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         return self.dfs(root)[0]
