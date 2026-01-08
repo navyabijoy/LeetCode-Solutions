af3ae0f99e8c90d82460b16ca36275413da5ba8a
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2026-01-09 02:58:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (14.82%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -8,26 +8,21 @@ class MyQueue:
         self.stack1.append(x)
 
     def pop(self) -> int:
-        while self.stack1:
-            self.stack2.append(self.stack1.pop())
-        val = self.stack2.pop()
-        while self.stack2:
-            self.stack1.append(self.stack2.pop())
-        return val
-
+        self._shift()
+        return self.stack2.pop()
 
     def peek(self) -> int:
-        while self.stack1:
-            self.stack2.append(self.stack1.pop())
-        val = self.stack2[-1]
-        while self.stack2:
-            self.stack1.append(self.stack2.pop())
-        return val
+        self._shift()
+        return self.stack2[-1]
         
 
     def empty(self) -> bool:
-        return len(self.stack1) == 0
-
+        return not self.stack1 and not self.stack2
+    
+    def _shift(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
