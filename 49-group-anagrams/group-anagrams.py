7b24b42ce14de283ef132162988bd314d0e31439
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2026-01-25 04:03:14 +0530

    Time: 11 ms (83.42%) | Memory: 22.1 MB (41.39%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        n = len(strs)
-        hashMap = defaultdict(list)
+        seen = defaultdict(list)
         for word in strs:
-            freq = [0] * (26)
-            for c in word:
-                freq[ord(c) - ord('a')] += 1
-            hashMap[tuple(freq)].append(word)
-
-        return list(hashMap.values())
+            sortedWord = "".join(sorted(word))
+            seen[sortedWord].append(word)
+        return list(seen.values())
