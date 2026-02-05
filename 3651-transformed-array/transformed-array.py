40dc55b639551bb6259b97661dbab733970db14d
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2026-02-05 23:27:39 +0530

    Time: 68 ms (13.45%) | Memory: 19.4 MB (10.76%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        for i in range(n):
+            target_index = ( i + nums[i]) % n
+            result[i] = nums[target_index]
+        return result
