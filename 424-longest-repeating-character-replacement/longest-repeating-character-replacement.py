17ee8928bc686fdec50284be9e88c462a2c8c62b
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2026-02-22 11:13:01 +0530

    Time: 87 ms (62.31%) | Memory: 19.6 MB (76.92%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,21 +1,24 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         left = 0
-        maxFreq = 0
         maxLen = 0
-        freq = [0] * 26
-        for right in range(len(s)):
-            freq[ord(s[right]) - ord('A')] += 1
-            maxFreq = max(maxFreq, freq[ord(s[right]) - ord('A')])
+        maxFreq = 0
+        freq = {}
 
-            while (right - left + 1) - maxFreq > k: 
-                # meaning, another character is becoming more freq 
-                freq[ord(s[left]) - ord('A')] -= 1
-                # maxFreq = 0
-                # for i in range(26):
-                #     maxFreq = max(maxFreq, freq[i])
-                left += 1
+        for right in range(len(s)):
+            if s[right] not in freq:
+                freq[s[right]] = 1
+            else:
+                freq[s[right]] += 1
             
-            # if (right - left + 1) <= k:
+            maxFreq = max(maxFreq, freq[s[right]])
+
+            while ((right - left + 1) - maxFreq) > k:
+                freq[s[left]] -= 1
+                if freq[s[left]] == 0:
+                    del freq[s[left]]
+                left += 1
+
             maxLen = max(maxLen, right - left + 1)
+            
         return maxLen
