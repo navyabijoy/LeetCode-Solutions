d8b9e75a7348d0bb6032fb8b48655d8f246eb2ac
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2026-04-16 02:11:02 +0530

    Time: 19 ms (77.55%) | Memory: 25.1 MB (94.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,16 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        pre_product = 1 #used 2 arrays, one to solve the product of numbers from the beginning 
-        suff_product = 1 #one to store the product of numbers from the last 
-        result = [0]*n #store the final result
+        res = [1] * n
 
+        prefix = 1
         for i in range(n):
-            result[i] = pre_product #iterates through the first array
-            pre_product *= nums[i] #number multiplies itself with the number before it e.g. [2,4,6,3] = [2,8,48,144]
-        for i in range(n-1,-1,-1): #reverse iterations
-            result[i] *= suff_product
-            suff_product *= nums[i] #[2,4,6,3] = [144,72,18,3]
-        return result
-
+            res[i] = prefix
+            prefix *= nums[i]
         
+        suffix = 1
+        for i in range(n-1,-1,-1):
+            res[i] *= suffix
+            suffix *= nums[i]
+        return res
