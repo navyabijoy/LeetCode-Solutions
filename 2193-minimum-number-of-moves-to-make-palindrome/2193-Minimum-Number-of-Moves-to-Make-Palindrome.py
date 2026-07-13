60291f5c98f0f51a9aa5fe69380f21efe9ce6847
Navya Bijoy <115248350+navyabijoy@users.noreply.github.com>
2026-07-13 11:04:44 +0530

    [2193] [Time Beats: 85.57%] [Memory Beats: 71.65%] - LeetPush

---
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-Minimum-Number-of-Moves-to-Make-Palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minMovesToMakePalindrome(self, s: str) -> int:
+        s = list(s)
+        ans = 0
+        while s:
+            i = s.index(s[-1])
+            if i == len(s) -1:
+                ans += i // 2
+            else:
+                ans += i
+                s.pop(i) # pops the first element
+            s.pop()
+        return ans
